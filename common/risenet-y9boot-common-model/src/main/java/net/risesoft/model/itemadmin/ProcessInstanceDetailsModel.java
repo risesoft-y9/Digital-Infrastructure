8ea75272e87dfe9dd5f2d3e145db57963dc6d8fb
mengjuhua <mengjuhua@risesoft.net>
2023-08-10 16:11:39 +0800

    实体类上的注释：@org.hibernate.annotations.Table.comment(" 全部替换为： @Comment("

--- common/risenet-y9boot-common-model/src/main/java/net/risesoft/model/itemadmin/ProcessInstanceDetailsModel.java
+++ common/risenet-y9boot-common-model/src/main/java/net/risesoft/model/itemadmin/ProcessInstanceDetailsModel.java
@@ -21,67 +21,67 @@ public class ProcessInstanceDetailsModel implements Serializable {
 
     private String id;
 
-    // @org.hibernate.annotations.Comment("流程实例id")
+    // @Comment("流程实例id")
     private String processInstanceId;
 
-    // @org.hibernate.annotations.Comment("流程编号")
+    // @Comment("流程编号")
     private String processSerialNumber;
 
-    // @org.hibernate.annotations.Comment("任务id")
+    // @Comment("任务id")
     private String taskId;
 
-    // @org.hibernate.annotations.Comment("任务名称")
+    // @Comment("任务名称")
     private String taskName;
 
-    // @org.hibernate.annotations.Comment("意见内容")
+    // @Comment("意见内容")
     private String opinionContent;
 
-    // @org.hibernate.annotations.Comment("系统英文名称")
+    // @Comment("系统英文名称")
     private String systemName;
 
-    // @org.hibernate.annotations.Comment("系统中文名称")
+    // @Comment("系统中文名称")
     private String systemCnName;
 
-    // @org.hibernate.annotations.Comment("事项id")
+    // @Comment("事项id")
     private String itemId;
 
-    // @org.hibernate.annotations.Comment("应用名称")
+    // @Comment("应用名称")
     private String appName;
 
-    // @org.hibernate.annotations.Comment("应用中文名称")
+    // @Comment("应用中文名称")
     private String appCnName;
 
-    // @org.hibernate.annotations.Comment("发送人id")
+    // @Comment("发送人id")
     private String senderId;
 
-    // @org.hibernate.annotations.Comment("发送人名称")
+    // @Comment("发送人名称")
     private String senderName;
 
-    // @org.hibernate.annotations.Comment("办理人id")
+    // @Comment("办理人id")
     private String assigneeId;
 
-    // @org.hibernate.annotations.Comment("办理人名称")
+    // @Comment("办理人名称")
     private String assigneeName;
 
-    // @org.hibernate.annotations.Comment("开始时间")
+    // @Comment("开始时间")
     private Date startTime;
 
-    // @org.hibernate.annotations.Comment("结束时间")
+    // @Comment("结束时间")
     private Date endTime;
 
-    // @org.hibernate.annotations.Comment("编号")
+    // @Comment("编号")
     private String serialNumber;
 
-    // @org.hibernate.annotations.Comment("文件标题")
+    // @Comment("文件标题")
     private String title;
 
-    // @org.hibernate.annotations.Comment("发起人")
+    // @Comment("发起人")
     private String userName;
 
-    // @org.hibernate.annotations.Comment("办件状态")
+    // @Comment("办件状态")
     private String itembox;// todo待办，doing在办，done办结
 
-    // @org.hibernate.annotations.Comment("详情链接")
+    // @Comment("详情链接")
     private String url;
 
 }
