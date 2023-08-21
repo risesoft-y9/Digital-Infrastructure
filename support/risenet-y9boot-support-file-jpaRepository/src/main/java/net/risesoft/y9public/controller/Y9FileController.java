84fd61c5a2135b7d5edcecbfa074dbccb2ca3f90
shidaobang <shidaobang@risesoft.net>
2023-08-21 11:33:54 +0800

    perf: 文件下载常用的响应 header

--- support/risenet-y9boot-support-file-jpaRepository/src/main/java/net/risesoft/y9public/controller/Y9FileController.java
+++ support/risenet-y9boot-support-file-jpaRepository/src/main/java/net/risesoft/y9public/controller/Y9FileController.java
@@ -3,29 +3,32 @@ package net.risesoft.y9public.controller;
 import java.io.IOException;
 
 import org.apache.commons.io.FilenameUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.ServletOutputStream;
-import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import net.risesoft.y9.util.Y9Assert;
-import net.risesoft.y9.util.mime.DownloadFileNameUtil;
+import net.risesoft.y9.util.mime.ContentDispositionUtil;
+import net.risesoft.y9.util.mime.MediaTypeUtils;
 import net.risesoft.y9public.entity.Y9FileStore;
 import net.risesoft.y9public.exception.Y9FileErrorCodeEnum;
 import net.risesoft.y9public.service.Y9FileStoreService;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class Y9FileController {
 
-    @Autowired
-    @Qualifier("y9FileStoreService")
-    private Y9FileStoreService y9FileStoreService;
+    private final Y9FileStoreService y9FileStoreService;
+    private final ServletContext servletContext;
 
     @RequestMapping(value = "/s/{realStoreFileName}")
     public void download(@PathVariable String realStoreFileName, HttpServletResponse response) throws Exception {
@@ -35,8 +38,9 @@ public class Y9FileController {
         Y9Assert.notNull(y9FileStore, Y9FileErrorCodeEnum.FILE_NOT_FOUND, id);
 
         try (ServletOutputStream out = response.getOutputStream()) {
-            response.setHeader("Content-disposition", DownloadFileNameUtil.standardize(y9FileStore.getFileName()));
-            response.setContentType("application/octet-stream");
+            response.setHeader("Content-disposition", ContentDispositionUtil.standardizeAttachment(y9FileStore.getFileName()));
+            response.setContentType(MediaTypeUtils.getMediaTypeForFileName(servletContext, y9FileStore.getFileName()).toString());
+            response.setContentLengthLong(y9FileStore.getFileSize());
 
             y9FileStoreService.downloadFileToOutputStream(id, out);
             out.flush();
