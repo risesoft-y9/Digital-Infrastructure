4d80457b3a533c7e3e6f393b0840fce06ac65bb2
dingzhaojun <dingzhaojun@risesoft.net>
2023-10-07 10:00:50 +0800

    compose update

--- docker/docker-compose-startup.bat
+++ docker/docker-compose-startup.bat
@@ -1,1 +1,6 @@
 docker compose -f compose-dev.yml up -d
+
+:: docker compose -f compose-portainer.yml up -d
+:: docker compose -f compose-prometheus.yml up -d
+:: docker compose -f compose-prometheus-exporter.yml up -d
+:: docker compose -f compose-grafana.yml up -d
