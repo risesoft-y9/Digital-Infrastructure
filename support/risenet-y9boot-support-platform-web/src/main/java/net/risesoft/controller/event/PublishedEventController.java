0af05e59bd1061f96c0ec2a55c75987522a7baca
mengjuhua <mengjuhua@risesoft.net>
2023-10-31 15:59:28 +0800

    修改事件操作接口，以及处理云运维人员密码修改错误问题
    (cherry picked from commit fce40661c36a5c4e84672505df56a36b03df031e)

--- support/risenet-y9boot-support-platform-web/src/main/java/net/risesoft/controller/event/PublishedEventController.java
+++ support/risenet-y9boot-support-platform-web/src/main/java/net/risesoft/controller/event/PublishedEventController.java
@@ -6,13 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
 import net.risesoft.log.annotation.RiseLog;
 import net.risesoft.pojo.Y9Page;
+import net.risesoft.pojo.Y9PageQuery;
 import net.risesoft.y9.Y9LoginUserHolder;
 import net.risesoft.y9public.entity.event.Y9PublishedEvent;
 import net.risesoft.y9public.service.event.Y9PublishedEventService;
@@ -36,23 +36,23 @@ public class PublishedEventController {
     /**
      * 获取操作事件分页列表
      *
-     * @param page 页数
-     * @param rows 条数
      * @param eventName 事件名称
      * @param eventDescription 事件描述
      * @param startTime 开始时间 格式 yyyy-MM-dd HH:mm:ss
      * @param endTime 结束时间 格式 yyyy-MM-dd HH:mm:ss
+     * @param pageQuery 分页
      * @return
-     * @since 9.6.1
+     *
+     * @since 9.6.3
      */
     @RiseLog(operationName = "分页获取操作事件")
     @RequestMapping("/pagePublishedEventList")
-    public Y9Page<Y9PublishedEvent> pagePublishedEventList(@RequestParam int page, @RequestParam int rows,
-        String eventName, String eventDescription, @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startTime,
-        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endTime) {
-        Page<Y9PublishedEvent> pageList = y9PublishedEventService.page(page, rows, Y9LoginUserHolder.getTenantId(),
-            eventName, eventDescription, startTime, endTime);
-        return Y9Page.success(page, pageList.getTotalPages(), pageList.getTotalElements(), pageList.getContent(),
-            "获取数据成功");
+    public Y9Page<Y9PublishedEvent> pagePublishedEventList(String eventName, String eventDescription,
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startTime,
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endTime, Y9PageQuery pageQuery) {
+        Page<Y9PublishedEvent> pageList = y9PublishedEventService.page(pageQuery.getPage(), pageQuery.getSize(),
+            Y9LoginUserHolder.getTenantId(), eventName, eventDescription, startTime, endTime);
+        return Y9Page.success(pageQuery.getPage(), pageList.getTotalPages(), pageList.getTotalElements(),
+            pageList.getContent(), "获取数据成功");
     }
 }
