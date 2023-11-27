055b0e59839716cb161a8b8d5ec065fd5d768b2a
mengjuhua <mengjuhua@risesoft.net>
2023-11-27 14:40:30 +0800

    上传个人签名

--- support/risenet-y9boot-support-platform-web/src/main/java/net/risesoft/controller/org/PersonController.java
+++ support/risenet-y9boot-support-platform-web/src/main/java/net/risesoft/controller/org/PersonController.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -414,12 +416,11 @@ public class PersonController {
     @RequestMapping("/showPersonPhoto")
     public void showPersonPhoto(@RequestParam @NotBlank String personId, HttpServletResponse response) {
         Y9Person person = y9PersonService.getById(personId);
-        try {
+        try (ServletOutputStream sos = response.getOutputStream()) {
             response.setContentType("image/jpg");
             Y9PersonExt ext = y9PersonExtService.findByPersonId(person.getId()).orElse(null);
             if (ext != null && ext.getPhoto() != null) {
                 InputStream inputStream = new ByteArrayInputStream(ext.getPhoto());
-                ServletOutputStream sos = response.getOutputStream();
                 byte[] b = new byte[1024];
                 int l;
                 while ((l = inputStream.read(b)) > -1) {
@@ -442,17 +443,17 @@ public class PersonController {
     @RequestMapping("/showPersonSign")
     public void showPersonSign(@RequestParam @NotBlank String personId, HttpServletResponse response) {
         Y9Person person = y9PersonService.getById(personId);
-        try {
-            response.setContentType("image/jpg");
+        try (ServletOutputStream out = response.getOutputStream()) {
             Y9PersonExt ext = y9PersonExtService.findByPersonId(person.getId()).orElse(null);
-            if (ext != null && ext.getPhoto() != null) {
+            if (ext != null && ext.getSign() != null) {
                 InputStream inputStream = new ByteArrayInputStream(ext.getSign());
-                ServletOutputStream sos = response.getOutputStream();
                 byte[] b = new byte[1024];
                 int l;
                 while ((l = inputStream.read(b)) > -1) {
-                    sos.write(b, 0, l);
+                    out.write(b, 0, l);
                 }
+                ImageIO.createImageOutputStream(out);
+                out.flush();
             }
         } catch (Exception e) {
             LOGGER.warn(e.getMessage(), e);
