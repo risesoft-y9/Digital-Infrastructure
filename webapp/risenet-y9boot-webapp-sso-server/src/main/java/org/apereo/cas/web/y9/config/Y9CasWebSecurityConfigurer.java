0660d5fc3874b4340e6d3aef4a1356d0d78b5a7d
dingzhaojun <dingzhaojun@risesoft.net>
2023-12-04 11:02:35 +0800

    sso update

--- webapp/risenet-y9boot-webapp-sso-server/src/main/java/org/apereo/cas/web/y9/config/Y9CasWebSecurityConfigurer.java
+++ webapp/risenet-y9boot-webapp-sso-server/src/main/java/org/apereo/cas/web/y9/config/Y9CasWebSecurityConfigurer.java
@@ -2,13 +2,9 @@ package org.apereo.cas.web.y9.config;
 
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apereo.cas.support.oauth.OAuth20Constants;
 import org.apereo.cas.web.CasWebSecurityConfigurer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ScopedProxyMode;
 
 /**
  * @author shidaobang
@@ -23,19 +19,9 @@ public class Y9CasWebSecurityConfigurer {
         return new CasWebSecurityConfigurer<>() {
             @Override
             public List<String> getIgnoredEndpoints() {
-                return List.of("/y9static", "/api");
+                return List.of("/y9static", "/api", "/login");
             }
         };
     }
     
-    /*@Bean
-    @RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
-    public CasWebSecurityConfigurer<Void> oauth20ProtocolEndpointConfigurer() {
-        return new CasWebSecurityConfigurer<>() {
-            @Override
-            public List<String> getIgnoredEndpoints() {
-                return List.of(StringUtils.prependIfMissing(OAuth20Constants.BASE_OAUTH20_URL, "/"), "/y9static", "/api");
-            }
-        };
-    }*/
 }
