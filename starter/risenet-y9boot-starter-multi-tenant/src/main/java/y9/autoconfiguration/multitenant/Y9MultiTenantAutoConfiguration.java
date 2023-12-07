706fa0920722d54017edd6571cfe83043f50bef2
shidaobang <shidaobang@risesoft.net>
2023-12-07 18:20:37 +0800

    refactor: 移除非必要依赖

--- starter/risenet-y9boot-starter-multi-tenant/src/main/java/y9/autoconfiguration/multitenant/Y9MultiTenantAutoConfiguration.java
+++ starter/risenet-y9boot-starter-multi-tenant/src/main/java/y9/autoconfiguration/multitenant/Y9MultiTenantAutoConfiguration.java
@@ -1,63 +1,64 @@
 package y9.autoconfiguration.multitenant;
 
-import java.util.Objects;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.hibernate.integrator.api.integrator.Y9TenantHibernateInfoHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import net.risesoft.init.TenantDataInitializer;
+import net.risesoft.kafka.MessageCommonListener;
 import net.risesoft.liquibase.Y9MultiTenantSpringLiquibase;
 import net.risesoft.schema.JpaSchemaUpdater;
 import net.risesoft.schema.LiquibaseSchemaUpdater;
+import net.risesoft.schema.NoneSchemaUpdater;
 import net.risesoft.schema.SchemaUpdater;
-import net.risesoft.y9.Y9Context;
 import net.risesoft.y9.configuration.Y9Properties;
-import net.risesoft.y9.json.Y9JsonUtil;
-import net.risesoft.y9.pubsub.constant.Y9CommonEventConst;
-import net.risesoft.y9.pubsub.event.Y9EventCommon;
-import net.risesoft.y9.pubsub.message.Y9MessageCommon;
 import net.risesoft.y9.tenant.datasource.Y9TenantDataSourceLookup;
-
-import y9.autoconfiguration.jpa.JpaPublicConfiguration;
-import y9.autoconfiguration.tenant.SpringMultiTenantConfiguration;
 
 /**
  * @author shidaobang
  * @date 2023/12/05
  * @since 9.6.3
  */
-@Configuration
-@AutoConfiguration(after = {SpringMultiTenantConfiguration.class, JpaPublicConfiguration.class},
-    before = {LiquibaseAutoConfiguration.class})
+@AutoConfiguration(after = KafkaAutoConfiguration.class)
 @EnableConfigurationProperties(Y9Properties.class)
 @RequiredArgsConstructor
 @Slf4j
 public class Y9MultiTenantAutoConfiguration {
 
-    private final Y9TenantDataSourceLookup y9TenantDataSourceLookup;
-
     @Bean("y9KafkaTemplate")
+    @Primary
     @ConditionalOnMissingBean(name = "y9KafkaTemplate")
     public KafkaTemplate<?, ?> y9KafkaTemplate(ProducerFactory<Object, Object> kafkaProducerFactory) {
         return new KafkaTemplate<>(kafkaProducerFactory);
     }
 
+    @ConditionalOnMissingBean(name = "y9TenantDataSourceLookup")
+    @Bean("y9TenantDataSourceLookup")
+    public Y9TenantDataSourceLookup y9TenantDataSourceLookup(@Qualifier("y9PublicDS") DruidDataSource ds,
+        Environment environment) {
+        return new Y9TenantDataSourceLookup(ds, environment.getProperty("y9.systemName"));
+    }
+
     @Bean
     @ConditionalOnBean(name = "y9MultiTenantSpringLiquibase")
-    public SchemaUpdater liquibaseDbUpdater(Y9MultiTenantSpringLiquibase y9MultiTenantSpringLiquibase,
+    public SchemaUpdater liquibaseDbUpdater(Y9TenantDataSourceLookup y9TenantDataSourceLookup,
+        Y9MultiTenantSpringLiquibase y9MultiTenantSpringLiquibase,
         @Autowired(required = false) TenantDataInitializer tenantDataInitializer,
         KafkaTemplate<String, String> y9KafkaTemplate) {
         return new LiquibaseSchemaUpdater(y9TenantDataSourceLookup, y9KafkaTemplate, y9MultiTenantSpringLiquibase,
@@ -66,39 +67,24 @@ public class Y9MultiTenantAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(name = "liquibaseDbUpdater")
-    public SchemaUpdater jpaDbUpdater(@Autowired(required = false) TenantDataInitializer tenantDataInitializer,
+    @ConditionalOnClass(value = Y9TenantHibernateInfoHolder.class)
+    public SchemaUpdater jpaDbUpdater(Y9TenantDataSourceLookup y9TenantDataSourceLookup,
+        @Autowired(required = false) TenantDataInitializer tenantDataInitializer,
         KafkaTemplate<String, String> y9KafkaTemplate) {
         return new JpaSchemaUpdater(y9TenantDataSourceLookup, y9KafkaTemplate, tenantDataInitializer);
     }
 
-    @KafkaListener(topics = {"y9_common_event"})
-    public void messageCommonListener4kafka(ConsumerRecord<String, String> data) {
-        String value = data.value();
-        try {
-            Y9MessageCommon msg = Y9JsonUtil.readValue(value, Y9MessageCommon.class);
-            String eventType = msg.getEventType();
+    @Bean
+    @ConditionalOnMissingBean(name = "jpaDbUpdater")
+    public SchemaUpdater noneSchemaUpdater(Y9TenantDataSourceLookup y9TenantDataSourceLookup,
+        @Autowired(required = false) TenantDataInitializer tenantDataInitializer,
+        KafkaTemplate<String, String> y9KafkaTemplate) {
+        return new NoneSchemaUpdater(y9TenantDataSourceLookup, tenantDataInitializer, y9KafkaTemplate);
+    }
 
-            if (Y9CommonEventConst.TENANT_DATASOURCE_SYNC.equals(eventType)) {
-                this.y9TenantDataSourceLookup.loadDataSources();
-                LOGGER.info(this.y9TenantDataSourceLookup.getSystemName() + ", 同步租户数据源信息, 成功！");
-                return;
-            }
-
-            if ((Y9CommonEventConst.TENANT_SYSTEM_REGISTERED.equals(eventType)
-                || Y9CommonEventConst.TENANT_SYSTEM_INITIALIZED.equals(eventType))
-                && !Objects.equals(Y9Context.getSystemName(), msg.getEventTarget())) {
-                // 对于非当前引入的系统的消息不处理
-                return;
-            }
-
-            Y9EventCommon event = new Y9EventCommon();
-            event.setEventType(msg.getEventType());
-            event.setEventObject(msg.getEventObject());
-            Y9Context.publishEvent(event);
-            LOGGER.info("[common]将消息中间件发过来的消息转换成spring的事件后发送：{}", event);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
+    @Bean
+    public MessageCommonListener messageCommonListener(Y9TenantDataSourceLookup y9TenantDataSourceLookup) {
+        return new MessageCommonListener(y9TenantDataSourceLookup);
     }
 
 }
