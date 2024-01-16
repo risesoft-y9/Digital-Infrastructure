5a23f10ac6e9ad9464c791cb0537f224435293d5
mengjuhua <mengjuhua@risesoft.net>
2024-01-16 11:00:11 +0800

    代码注释

--- rest/risenet-y9boot-rest-interface-platform/src/main/java/net/risesoft/api/v0/org/OrgUnitApi.java
+++ rest/risenet-y9boot-rest-interface-platform/src/main/java/net/risesoft/api/v0/org/OrgUnitApi.java
@@ -42,7 +42,7 @@ public interface OrgUnitApi {
      *
      * @param tenantId 租户id
      * @param orgUnitId 组织节点唯一标识(可能是机构ID,也可能是部门ID)
-     * @return List<Department> 部门对象集合
+     * @return List&lt;Department&gt; 部门对象集合
      * @since 9.6.0
      */
     @GetMapping("/getDeptTrees")
@@ -104,7 +104,7 @@ public interface OrgUnitApi {
      * @param orgUnitId 组织节点唯一标识
      * @param treeType 树的类型:tree_type_org(组织机构)，tree_type_dept（部门） tree_type_group（用户组）, tree_type_position（岗位）
      *            tree_type_person（人员）, tree_type_bureau（委办局）
-     * @return List<OrgUnit> 组织节点对象集合
+     * @return List&lt;OrgUnit&gt; 组织节点对象集合
      * @since 9.6.0
      */
     @GetMapping("/getSubTree")
@@ -118,7 +118,7 @@ public interface OrgUnitApi {
      * @param name 组织节点名称
      * @param treeType 树的类型:tree_type_org(组织机构)，tree_type_dept（部门），tree_type_group（用户组），tree_type_position（岗位）
      *            tree_type_person（人员），tree_type_bureau（委办局）
-     * @return List<OrgUnit> 组织节点对象集合
+     * @return List&lt;OrgUnit&gt; 组织节点对象集合
      * @since 9.6.0
      */
     @GetMapping("/treeSearch")
@@ -133,7 +133,7 @@ public interface OrgUnitApi {
      * @param dnName 路径名称
      * @param treeType 节点树的类型:tree_type_org(组织机构)，tree_type_dept（部门） tree_type_group（用户组）, tree_type_position（岗位）
      *            tree_type_person（人员）, tree_type_bureau（委办局）
-     * @return List<OrgUnit> 组织节点对象集合
+     * @return List&lt;OrgUnit&gt; 组织节点对象集合
      * @since 9.6.0
      */
     @GetMapping("/treeSearchByDn")
