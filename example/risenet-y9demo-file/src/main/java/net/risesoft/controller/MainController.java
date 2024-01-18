f363ac1be25e57e365de90200f62efb82115bf54
qinman <qinman@risesoft.net>
2024-01-18 11:02:55 +0800

    添加文件上传示例工程
    (cherry picked from commit 3f77096c40a9c347428395188080de57e9673050)

---
+++ example/risenet-y9demo-file/src/main/java/net/risesoft/controller/MainController.java
@@ -0,0 +1,86 @@
+package net.risesoft.controller;
+
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import net.risesoft.y9public.entity.Y9FileStore;
+import net.risesoft.y9public.service.Y9FileStoreService;
+
+@RestController
+@RequestMapping(value = "/main")
+public class MainController {
+
+    Logger log = LoggerFactory.getLogger(MainController.class);
+
+    @Autowired
+    private Y9FileStoreService y9FileStoreService;
+
+    /**
+     * 上传文件
+     * 
+     * @param file
+     */
+    @PostMapping(value = "/uploadFile")
+    public void uploadFile(MultipartFile file) {
+        try {
+            String fullPath = "/aaaa/bbbb";
+            Y9FileStore y9FileStore = y9FileStoreService.uploadFile(file, fullPath, file.getOriginalFilename());
+            log.info("fileStoreId:{}", y9FileStore.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 根据文件唯一标示下载文件
+     * 
+     * @param fileStoreId
+     * @param response
+     * @param request
+     */
+    @RequestMapping(value = "/download")
+    public void download(@RequestParam String fileStoreId, HttpServletResponse response, HttpServletRequest request) {
+        try {
+            Y9FileStore y9FileStore = y9FileStoreService.getById(fileStoreId);
+            String title = y9FileStore.getFileName();
+            title = java.net.URLEncoder.encode(title, "UTF-8");
+            title = StringUtils.replace(title, "+", "%20");// 替换空格
+            response.reset();
+            response.setHeader("Content-disposition", "attachment; filename=\"" + title + "\"");
+            response.setHeader("Content-type", "text/html;charset=UTF-8");
+            response.setContentType("application/octet-stream");
+            OutputStream out = response.getOutputStream();
+            y9FileStoreService.downloadFileToOutputStream(fileStoreId, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 根据文件唯一标示删除文件
+     * 
+     * @param fileStoreId
+     */
+    @RequestMapping(value = "/deleteFile")
+    public void deleteFile(@RequestParam String fileStoreId) {
+        try {
+            y9FileStoreService.deleteFile(fileStoreId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ example/risenet-y9demo-file/src/main/java/net/risesoft/demos/web/User.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.risesoft.demos.web;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
