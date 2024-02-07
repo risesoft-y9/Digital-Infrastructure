3aabe30a9a4e1095b2842075bad10d6336e6cf41
shidaobang <shidaobang@risesoft.net>
2024-02-07 09:25:32 +0800

    fix: 部署到外部 servlet 容器不能自动注册服务的问题
    (cherry picked from commit 24b73d8622b60d6466e4cb5c9ea3eae4031a08d1)

---
+++ common/risenet-y9boot-common-nacos/src/main/java/y9/autoconfiguration/NacosAutoConfiguration.java
@@ -0,0 +1,28 @@
+package y9.autoconfiguration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWarDeployment;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.cloud.nacos.registry.NacosAutoServiceRegistration;
+
+/**
+ * nacos 自动配置
+ *
+ * @author shidaobang
+ * @date 2024/02/06
+ */
+@Configuration(proxyBeanMethods = false)
+public class NacosAutoConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.cloud.nacos.discovery.enabled", matchIfMissing = true)
+    @ConditionalOnWarDeployment
+    public NacosAutoServiceRegistrationListener nacosAutoServiceRegistrationListener(
+        NacosAutoServiceRegistration nacosAutoServiceRegistration, ServerProperties serverProperties) {
+        return new NacosAutoServiceRegistrationListener(nacosAutoServiceRegistration, serverProperties);
+    }
+
+}
---
+++ common/risenet-y9boot-common-nacos/src/main/java/y9/autoconfiguration/NacosAutoServiceRegistrationListener.java
@@ -0,0 +1,28 @@
+package y9.autoconfiguration;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+
+import com.alibaba.cloud.nacos.registry.NacosAutoServiceRegistration;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * nacos 自动服务注册监听器
+ *
+ * @author shidaobang
+ * @date 2024/02/06
+ */
+@RequiredArgsConstructor
+public class NacosAutoServiceRegistrationListener implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final NacosAutoServiceRegistration registration;
+    private final ServerProperties serverProperties;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        registration.setPort(serverProperties.getPort());
+        registration.start();
+    }
+}
