263c4cdb39c2bc6ccf3dadea5e93e5d4aaa4cf3a
mengjuhua <mengjuhua@risesoft.net>
2024-02-28 18:02:18 +0800

    移除多余的属性

--- y9-digitalbase-common/risenet-y9boot-properties/src/main/java/net/risesoft/y9/configuration/app/y9digitalbase/Y9DigitalBaseProperties.java
+++ y9-digitalbase-common/risenet-y9boot-properties/src/main/java/net/risesoft/y9/configuration/app/y9digitalbase/Y9DigitalBaseProperties.java
@@ -9,14 +9,6 @@ public class Y9DigitalBaseProperties {
 
     private String systemName = "risedigitalBase";
 
-    /** 人员管理的菜单ID */
-    private String menuId = "54f055c4368c485ab762dfd88c5d70e5";
-
-    /** 下载临时存放目录 */
-    private String downloadTempDir = "//home//y9admin";
-
-    private String saveSoaEable = "false";
-
     private String orclNewTableSpace = "/u01/app/oracle/oradata/orcl/";
 
     private String y9IsvGuid = "993f08e4a9624c7d82b0bd0766f45f10";
