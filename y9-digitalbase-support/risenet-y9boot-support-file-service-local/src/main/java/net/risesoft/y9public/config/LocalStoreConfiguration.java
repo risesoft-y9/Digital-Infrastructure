8b0ef2ad65a2f132cc97925958c5a12649fa3e23
shidaobang <shidaobang@risesoft.net>
2024-04-22 16:41:27 +0800

    feat: 文件上传至本地文件目录

---
+++ y9-digitalbase-support/risenet-y9boot-support-file-service-local/src/main/java/net/risesoft/y9public/config/LocalStoreConfiguration.java
@@ -0,0 +1,33 @@
+package net.risesoft.y9public.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.risesoft.y9.configuration.feature.file.local.Y9LocalProperties;
+import net.risesoft.y9public.service.StoreService;
+import net.risesoft.y9public.service.impl.LocalStoreServiceImpl;
+
+/**
+ * 本地文件存储配置类
+ *
+ * @author shidaobang
+ * @date 2024/04/22
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(name = "y9.feature.file.local.enabled", havingValue = "true", matchIfMissing = true)
+public class LocalStoreConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "y9.feature.file.local")
+    public Y9LocalProperties y9LocalProperties() {
+        return new Y9LocalProperties();
+    }
+
+    @Bean
+    public StoreService localStoreService(Y9LocalProperties y9LocalProperties) {
+        return new LocalStoreServiceImpl(y9LocalProperties);
+    }
+
+}
