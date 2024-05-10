f4ad7df0ce6b22365b528e9eb633d6b722fbe75a
shidaobang <shidaobang@risesoft.net>
2024-05-10 18:14:01 +0800

    refactor: 雪花算法加入默认构造方法，使用 hutool 工具类生成数据中心及机器号
    (cherry picked from commit 2e8cc1f6ee46d3bd967bcf01b6bfd22cc23b9840)

--- y9-digitalbase-starter/risenet-y9boot-starter-idgenerator/src/main/java/y9/autoconfiguration/id/generator/IdGeneratorConfiguration.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-idgenerator/src/main/java/y9/autoconfiguration/id/generator/IdGeneratorConfiguration.java
@@ -2,9 +2,7 @@ package y9.autoconfiguration.id.generator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
 
-import net.risesoft.id.Y9IpUtil;
 import net.risesoft.id.Y9SpringContext;
 import net.risesoft.id.impl.SnowflakeIdGenerator;
 import net.risesoft.id.impl.TimeBasedUuidGenerator;
@@ -22,17 +20,7 @@ public class IdGeneratorConfiguration {
     }
 
     @Bean
-    public SnowflakeIdGenerator snowflakeIdGenerator(RedisTemplate<Object, Object> redisTemplate) {
-        long machineId = 0;
-        String ip = Y9IpUtil.getIp();
-        if (redisTemplate.hasKey("snowflake:" + ip)) {
-            machineId = (Long)redisTemplate.opsForValue().get("snowflake:" + ip);
-        } else {
-            int count = redisTemplate.keys("snowflake:*").size();
-            machineId = count + 1;
-            redisTemplate.opsForValue().set("snowflake:" + ip, machineId);
-        }
-
-        return new SnowflakeIdGenerator(machineId);
+    public SnowflakeIdGenerator snowflakeIdGenerator() {
+        return new SnowflakeIdGenerator();
     }
 }
