078bad83e3685879ce7c35806520e4f4b45c6598
zhangchongjie <zhangchongjie@risesoft.net>
2024-05-13 15:16:58 +0800

    fix: 引用错误

--- y9-digitalbase-webapp/y9-module-platform/risenet-y9boot-support-platform-jpa-repository/src/main/java/net/risesoft/y9public/entity/event/Y9OrgSyncRole.java
+++ y9-digitalbase-webapp/y9-module-platform/risenet-y9boot-support-platform-jpa-repository/src/main/java/net/risesoft/y9public/entity/event/Y9OrgSyncRole.java
@@ -1,9 +1,9 @@
 package net.risesoft.y9public.entity.event;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import org.hibernate.annotations.Comment;
 
