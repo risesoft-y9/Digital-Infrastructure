4826a1fd2c6343cd0a721ad6f0a85541f0f30d72
qinman <qinman@risesoft.net>
2024-05-16 11:48:26 +0800

    完善检查用户过滤器

--- y9-digitalbase-webapp/y9-module-platform/risenet-y9boot-webapp-platform/src/main/java/net/risesoft/config/CheckUserLoginFilter4Platform.java
+++ y9-digitalbase-webapp/y9-module-platform/risenet-y9boot-webapp-platform/src/main/java/net/risesoft/config/CheckUserLoginFilter4Platform.java
@@ -1,22 +1,18 @@
 package net.risesoft.config;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import lombok.extern.slf4j.Slf4j;
+import net.risesoft.entity.Y9Manager;
+import net.risesoft.model.user.UserInfo;
+import net.risesoft.service.org.Y9ManagerService;
+import net.risesoft.y9.Y9Context;
+import net.risesoft.y9.Y9LoginUserHolder;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import lombok.extern.slf4j.Slf4j;
-
-import net.risesoft.model.user.UserInfo;
-import net.risesoft.y9.Y9LoginUserHolder;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author dingzhaojun
@@ -28,33 +24,31 @@ import net.risesoft.y9.Y9LoginUserHolder;
 public class CheckUserLoginFilter4Platform implements Filter {
 
     @Override
-    public void destroy() {}
+    public void destroy() {
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
-        throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest)servletRequest;
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpSession session = request.getSession();
-        HttpServletResponse response = (HttpServletResponse)servletResponse;
-        // 在risenet-y9boot-3rd-cas-client工程的AssertionThreadLocalFilter类中，已经往session中保存了以下对象：
-        // tenantName、loginName、loginPerson、tenantId、y9User等
-        // 同时Y9LoginUserHolder也设置了tenantName、tenantId、person、Map<String, Object>属性。
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        // 在risenet-y9boot-starter-sso-oauth2-resource工程的Y9Oauth2ResourceFilter类中，已经往session中保存了以下对象：
+        // access_token、userInfo、loginName、positionId、deptId
+        // 同时Y9LoginUserHolder也设置了positionId、tenantId、tenantName、tenantShortName、UserInfo
         try {
-            UserInfo loginUser = (UserInfo)session.getAttribute("loginUser");
+            UserInfo loginUser = (UserInfo) session.getAttribute("loginUser");
             if (loginUser == null) {
                 loginUser = Y9LoginUserHolder.getUserInfo();
             }
             if (loginUser == null) {
                 throw new RuntimeException("No user was found in httpsession !!!");
             } else {
-                // Boolean isTenantPerson = loginUser.isGlobalManager();
-                // Y9DepartmentPropService y9DepartmentPropService = Y9Context.getBean(Y9DepartmentPropService.class);
-                // List<Y9DepartmentProp> list =
-                // y9DepartmentPropService.findByOrgBaseIdAndCategory(loginUser.getPersonId(),
-                // Y9DepartmentPropCategoryEnum.ADMIN.getCategory());
-                // if (!isTenantPerson && list.size() == 0) {
-                // throw new RuntimeException("This user is not an administrator, without permission !!!");
-                // }
+                Y9ManagerService y9ManagerService = Y9Context.getBean(Y9ManagerService.class);
+                Y9Manager y9Manager = y9ManagerService.getById(loginUser.getPersonId());
+                if (null == y9Manager) {
+                    throw new RuntimeException("This user is not an administrator, without permission !!!");
+                }
             }
         } catch (RuntimeException e) {
             LOGGER.info(e.getMessage(), e);
@@ -68,6 +62,7 @@ public class CheckUserLoginFilter4Platform implements Filter {
     }
 
     @Override
-    public void init(final FilterConfig filterConfig) throws ServletException {}
+    public void init(final FilterConfig filterConfig) throws ServletException {
+    }
 
 }
