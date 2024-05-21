9346616df050cb4679b28620e12d965c0c156fdb
qinman <qinman@risesoft.net>
2024-05-21 17:58:37 +0800

    单元测试

---
+++ y9-digitalbase-webapp/y9-module-platform/risenet-y9boot-support-platform-web/src/test/java/net/risesoft/TestPlatform.java
@@ -0,0 +1,17 @@
+package net.risesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dingzhaojun
+ * @author qinman
+ * @author mengjuhua
+ * @date 2022/2/10
+ */
+@SpringBootApplication
+public class TestPlatform {
+    public static void main(String[] args) {
+        SpringApplication.run(TestPlatform.class, args);
+    }
+}
