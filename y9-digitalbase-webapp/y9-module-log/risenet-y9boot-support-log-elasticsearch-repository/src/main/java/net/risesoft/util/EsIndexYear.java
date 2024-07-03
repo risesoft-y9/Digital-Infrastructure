f58049ce3213a387ad1366c8f4bfed3da52fa6ff
shidaobang <shidaobang@risesoft.net>
2024-07-03 15:32:14 +0800

    fix: elasticsearch 报错

--- y9-digitalbase-webapp/y9-module-log/risenet-y9boot-support-log-elasticsearch-repository/src/main/java/net/risesoft/util/EsIndexYear.java
+++ y9-digitalbase-webapp/y9-module-log/risenet-y9boot-support-log-elasticsearch-repository/src/main/java/net/risesoft/util/EsIndexYear.java
@@ -1,7 +1,10 @@
 package net.risesoft.util;
 
+import org.springframework.stereotype.Component;
+
 import cn.hutool.core.date.DateTime;
 
+@Component
 public class EsIndexYear {
 
     public String getYearStr() {
