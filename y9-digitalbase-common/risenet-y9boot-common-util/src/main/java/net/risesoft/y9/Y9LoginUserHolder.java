99271c9ae708a3188411dd3c31a12f9db862ce85
zhangchongjie <zhangchongjie@risesoft.net>
2024-07-30 17:10:29 +0800

    Y9LoginUserHolder添加OrgUnit类型，OrgUnitApi添加根据orgUnitId获取岗位或人员接口

--- y9-digitalbase-common/risenet-y9boot-common-util/src/main/java/net/risesoft/y9/Y9LoginUserHolder.java
+++ y9-digitalbase-common/risenet-y9boot-common-util/src/main/java/net/risesoft/y9/Y9LoginUserHolder.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
 
+import net.risesoft.model.platform.OrgUnit;
 import net.risesoft.model.platform.Person;
 import net.risesoft.model.platform.Position;
 import net.risesoft.model.user.UserInfo;
@@ -28,6 +29,9 @@ public abstract class Y9LoginUserHolder {
     private static final TransmittableThreadLocal<Position> POSITION_HOLDER = new TransmittableThreadLocal<Position>();
     private static final TransmittableThreadLocal<String> POSITION_ID_HOLDER = new TransmittableThreadLocal<String>();
 
+    private static final TransmittableThreadLocal<OrgUnit> ORGUNIT_HOLDER = new TransmittableThreadLocal<OrgUnit>();
+    private static final TransmittableThreadLocal<String> ORGUNIT_ID_HOLDER = new TransmittableThreadLocal<String>();
+
     private static final TransmittableThreadLocal<Map<String, Object>> MAP_HOLDER =
         new TransmittableThreadLocal<Map<String, Object>>();
 
@@ -43,6 +47,9 @@ public abstract class Y9LoginUserHolder {
         POSITION_HOLDER.remove();
         POSITION_ID_HOLDER.remove();
 
+        ORGUNIT_HOLDER.remove();
+        ORGUNIT_ID_HOLDER.remove();
+
         MAP_HOLDER.remove();
     }
 
@@ -55,47 +62,38 @@ public abstract class Y9LoginUserHolder {
         return MAP_HOLDER.get();
     }
 
-    public static String getPersonId() {
-        return PERSON_ID_HOLDER.get();
-    }
-
-    public static Position getPosition() {
-        return POSITION_HOLDER.get();
-    }
-
-    public static String getPositionId() {
-        return POSITION_ID_HOLDER.get();
-    }
-
-    public static String getTenantId() {
-        return TENANT_ID_HOLDER.get();
-    }
-
-    public static String getTenantName() {
-        return TENANT_NAME_HOLDER.get();
-    }
-
-    public static String getTenantShortName() {
-        return TENANT_SHORT_NAME_HOLDER.get();
-    }
-
-    public static UserInfo getUserInfo() {
-        return USER_INFO_HOLDER.get();
-    }
-
     public static void setMap(final Map<String, Object> map) {
         MAP_HOLDER.set(map);
     }
 
-    public static void setPerson(final Person person) {
-        USER_INFO_HOLDER.set(person.toUserInfo());
-        PERSON_ID_HOLDER.set(person.getId());
-        TENANT_ID_HOLDER.set(person.getTenantId());
-        // POSITION_ID_HOLDER.set(person.getPositionId());
+    public static OrgUnit getOrgUnit() {
+        return ORGUNIT_HOLDER.get();
+    }
+
+    public static void setOrgUnit(final OrgUnit orgUnit) {
+        ORGUNIT_HOLDER.set(orgUnit);
+        ORGUNIT_ID_HOLDER.set(orgUnit.getId());
+        TENANT_ID_HOLDER.set(orgUnit.getTenantId());
+    }
+
+    public static String getOrgUnitId() {
+        return ORGUNIT_ID_HOLDER.get();
+    }
+
+    public static void setOrgUnitId(final String orgUnitId) {
+        ORGUNIT_ID_HOLDER.set(orgUnitId);
+    }
+
+    public static String getPersonId() {
+        return PERSON_ID_HOLDER.get();
     }
 
     public static void setPersonId(final String personId) {
         PERSON_ID_HOLDER.set(personId);
+    }
+
+    public static Position getPosition() {
+        return POSITION_HOLDER.get();
     }
 
     public static void setPosition(final Position position) {
@@ -103,20 +101,40 @@ public abstract class Y9LoginUserHolder {
         POSITION_ID_HOLDER.set(position.getId());
     }
 
+    public static String getPositionId() {
+        return POSITION_ID_HOLDER.get();
+    }
+
     public static void setPositionId(final String positionId) {
         POSITION_ID_HOLDER.set(positionId);
+    }
+
+    public static String getTenantId() {
+        return TENANT_ID_HOLDER.get();
     }
 
     public static void setTenantId(final String tenantId) {
         TENANT_ID_HOLDER.set(tenantId);
     }
 
+    public static String getTenantName() {
+        return TENANT_NAME_HOLDER.get();
+    }
+
     public static void setTenantName(final String tenantName) {
         TENANT_NAME_HOLDER.set(tenantName);
     }
 
+    public static String getTenantShortName() {
+        return TENANT_SHORT_NAME_HOLDER.get();
+    }
+
     public static void setTenantShortName(final String tenantShortName) {
         TENANT_SHORT_NAME_HOLDER.set(tenantShortName);
+    }
+
+    public static UserInfo getUserInfo() {
+        return USER_INFO_HOLDER.get();
     }
 
     public static void setUserInfo(final UserInfo userInfo) {
@@ -125,6 +143,13 @@ public abstract class Y9LoginUserHolder {
         PERSON_ID_HOLDER.set(userInfo.getPersonId());
         TENANT_ID_HOLDER.set(userInfo.getTenantId());
         POSITION_ID_HOLDER.set(userInfo.getPositionId());
+    }
+
+    public static void setPerson(final Person person) {
+        USER_INFO_HOLDER.set(person.toUserInfo());
+        PERSON_ID_HOLDER.set(person.getId());
+        TENANT_ID_HOLDER.set(person.getTenantId());
+        // POSITION_ID_HOLDER.set(person.getPositionId());
     }
 
 }
