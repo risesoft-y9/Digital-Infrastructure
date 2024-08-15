939fff00fc4fc2abd84b37e20c51ee5d0dde235e
shidaobang <shidaobang@risesoft.net>
2024-08-15 11:37:29 +0800

    refactor: 移除重复定义的 RequestContextFilter

--- y9-digitalbase-webapp/y9-module-platform/risenet-y9boot-webapp-platform/src/main/java/net/risesoft/config/WebMvcConfig.java
+++ y9-digitalbase-webapp/y9-module-platform/risenet-y9boot-webapp-platform/src/main/java/net/risesoft/config/WebMvcConfig.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.filter.RequestContextFilter;
 
 import net.risesoft.y9.Y9Context;
 import net.risesoft.y9.configuration.Y9Properties;
@@ -29,14 +27,6 @@ import net.risesoft.y9.configuration.Y9Properties;
 @EnableConfigurationProperties(Y9Properties.class)
 @EnableAsync(proxyTargetClass = true, mode = AdviceMode.ASPECTJ)
 public class WebMvcConfig {
-
-    // starter-log工程用到了RequestContextHolder
-    // https://github.com/spring-projects/spring-boot/issues/2637
-    // https://github.com/spring-projects/spring-boot/issues/4331
-    @Bean
-    public static RequestContextFilter requestContextFilter() {
-        return new OrderedRequestContextFilter();
-    }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
     @DependsOn("y9Context")
