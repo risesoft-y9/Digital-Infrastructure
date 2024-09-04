b76e9b43be4630fd7bf8b79998116b612810aaef
mengjuhua <mengjuhua@risesoft.net>
2024-09-04 11:19:37 +0800

    feat：增加elasticsearch示例工程
    (cherry picked from commit 71e5f169394e884429abfb890fa9fe94d068d3e7)

---
+++ y9-digitalbase-example/risenet-y9demo-elasticsearch/src/main/java/net/risesoft/RisenetY9demoElkApplication.java
@@ -0,0 +1,13 @@
+package net.risesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisenetY9demoElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RisenetY9demoElkApplication.class, args);
+    }
+
+}
