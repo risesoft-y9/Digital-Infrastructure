8260a346b6768b05e438580130f170885ed8a51f
shidaobang <shidaobang@risesoft.net>
2024-09-13 14:49:50 +0800

    feat: 添加日志上报的 demo 工程

---
+++ y9-digitalbase-example/risenet-y9demo-log/src/main/java/net/risesoft/log/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.risesoft.log;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Y9DemoLogApplication.class);
+    }
+
+}
---
+++ y9-digitalbase-example/risenet-y9demo-log/src/main/java/net/risesoft/log/Y9DemoLogApplication.java
@@ -0,0 +1,13 @@
+package net.risesoft.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y9DemoLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y9DemoLogApplication.class, args);
+    }
+
+}
