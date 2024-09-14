5851c19199af329c4da2aae796bfedf02c229c82
mengjuhua <mengjuhua@risesoft.net>
2024-09-14 17:16:20 +0800

    remove: 移除 fileManager 中文件浏览前端组件 RichFilemanager

--- y9-digitalbase-module/y9-module-filemanager/risenet-y9boot-webapp-filemanager/src/main/java/net/risesoft/y9public/controller/Y9FileController.java
+++ y9-digitalbase-module/y9-module-filemanager/risenet-y9boot-webapp-filemanager/src/main/java/net/risesoft/y9public/controller/Y9FileController.java
@@ -1,9 +1,7 @@
 package net.risesoft.y9public.controller;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.ServletOutputStream;
@@ -13,17 +11,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.io.FileUtils;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.fabriceci.fmc.error.FMInitializationException;
-import com.fabriceci.fmc.error.FileManagerException;
-import com.fabriceci.fmc.impl.LocalFileManager;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -36,11 +29,7 @@ import net.risesoft.y9public.repository.Y9FileStoreRepository;
 @RequiredArgsConstructor
 public class Y9FileController {
 
-    private String fileRoot = "d:/y9config";
-
     private String fileStoreRoot = "d:/y9config/y9filestore";
-
-    private LocalFileManager localFileManager = null;
 
     private final Y9FileStoreRepository fileRepository;
 
@@ -66,23 +55,14 @@ public class Y9FileController {
 
     @RequestMapping(value = "/s/{realStoreFileName}")
     public void download(@PathVariable String realStoreFileName, HttpServletRequest request,
-        HttpServletResponse response) throws Exception {
+                         HttpServletResponse response) throws Exception {
         downloadFile(realStoreFileName, request, response);
     }
 
-    // 等同于 /api?mode=download&path=
-    @RequestMapping(value = "/download")
-    public void download(String path, HttpServletResponse response) throws Exception {
-        String s = path;
-        if (!s.startsWith("/")) {
-            s = "/" + s;
-        }
-        localFileManager.actionDownload(response, s);
-    }
-
     @RequestMapping(value = "/s")
-    public void download2(@RequestParam String realStoreFileName, HttpServletResponse response) throws Exception {
-        download(realStoreFileName, response);
+    public void download2(@RequestParam String realStoreFileName, HttpServletRequest request,
+                          HttpServletResponse response) throws Exception {
+        downloadFile(realStoreFileName, request, response);
     }
 
     /**
@@ -94,7 +74,7 @@ public class Y9FileController {
      */
     @RequestMapping(value = "/file/{realStoreFileName}")
     public void downloadFile(@PathVariable String realStoreFileName, HttpServletRequest request,
-        HttpServletResponse response) throws Exception {
+                             HttpServletResponse response) throws Exception {
         ServletOutputStream out = response.getOutputStream();
         try {
             String id = realStoreFileName;
@@ -111,10 +91,10 @@ public class Y9FileController {
                 if (file.exists()) {
                     String userAgent = request.getHeader("User-Agent");
                     if (userAgent.contains("MSIE") || userAgent.contains("Trident")) {
-                        fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8);
+                        fileName = URLEncoder.encode(fileName, "UTF-8");
                         fileName = fileName.replaceAll("\\+", "%20");
                     } else {
-                        fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), "ISO8859-1");
+                        fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");
                     }
 
                     response.reset();
@@ -130,13 +110,13 @@ public class Y9FileController {
 
             response.reset();
             String err = "文件不存在或路径不正确";
-            out.write(err.getBytes(StandardCharsets.UTF_8));
+            out.write(err.getBytes("UTF-8"));
         } catch (Exception e) {
             e.printStackTrace();
 
             response.reset();
             String err = "错误信息：" + e.getMessage();
-            out.write(err.getBytes(StandardCharsets.UTF_8));
+            out.write(err.getBytes("UTF-8"));
         } finally {
             out.flush();
             out.close();
@@ -145,42 +125,24 @@ public class Y9FileController {
 
     @RequestMapping(value = "/files")
     public void downloadFile2(@RequestParam String realStoreFileName, HttpServletRequest request,
-        HttpServletResponse response) throws Exception {
+                              HttpServletResponse response) throws Exception {
         downloadFile(realStoreFileName, request, response);
     }
 
     @RequestMapping(value = "/files/{realStoreFileName}")
     public void downloadFiles(@PathVariable String realStoreFileName, HttpServletRequest request,
-        HttpServletResponse response) throws Exception {
+                              HttpServletResponse response) throws Exception {
         download(realStoreFileName, request, response);
-    }
-
-    @RequestMapping(value = "/api")
-    public void handleRequest(HttpServletRequest request, HttpServletResponse response)
-        throws IOException, FileManagerException {
-        localFileManager.handleRequest(request, response);
-    }
-
-    @GetMapping(value = {"/index", ""})
-    public String index() {
-        return "index";
     }
 
     @PostConstruct
     public void init() {
         this.fileStoreRoot = environment.getProperty("y9.app.fileManager.fileRoot", "d:/y9config/y9filestore");
-        try {
-            localFileManager = new LocalFileManager();
-            fileRoot = localFileManager.getPropertiesConfig().getProperty("fileRoot", fileRoot);
-        } catch (FMInitializationException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
     }
 
     @RequestMapping(value = "/rest/retrieveFileStream")
     public void retrieveFileStream(String fullPath, String fileName, HttpServletRequest request,
-        HttpServletResponse response) throws Exception {
+                                   HttpServletResponse response) throws Exception {
         ServletOutputStream out = response.getOutputStream();
         try {
             File path = new File(this.fileStoreRoot + fullPath);
@@ -188,10 +150,10 @@ public class Y9FileController {
             if (file.exists()) {
                 String userAgent = request.getHeader("User-Agent");
                 if (userAgent.contains("MSIE") || userAgent.contains("Trident")) {
-                    fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8);
+                    fileName = URLEncoder.encode(fileName, "UTF-8");
                     fileName = fileName.replaceAll("\\+", "%20");
                 } else {
-                    fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), "ISO8859-1");
+                    fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");
                 }
 
                 response.reset();
--- y9-digitalbase-module/y9-module-filemanager/risenet-y9boot-webapp-filemanager/src/main/resources/static/richFileManager/config/filemanager.init.js
+++
@@ -1,5 +0,0 @@
-$('.fm-container').richFilemanager({
-    // options for the plugin initialization step and callback functions, see:
-    // https://github.com/servocoder/RichFilemanager/wiki/Configuration-options#plugin-parameters
-	"baseUrl": ctx + "/richFileManager"
-});
