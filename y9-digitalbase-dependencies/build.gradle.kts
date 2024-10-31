e3a46cbbf3f9a6744ddc38213a7c3fe81efa814d
dingzhaojun <dingzhaojun@risesoft.net>
2024-10-31 11:46:00 +0800

    继续完成从build.gradle到build.gradle.kts的转换.

--- y9-digitalbase-dependencies/build.gradle.kts
+++ y9-digitalbase-dependencies/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-platform")
+    id("buildlogic.y9-repository")
 }
 
 javaPlatform {
@@ -9,4 +10,7 @@ javaPlatform {
 description = "y9-digitalbase-dependencies"
 
 dependencies {
+    constraints {
+        api(libs.risenet.y9boot.properties)
+    }
 }
