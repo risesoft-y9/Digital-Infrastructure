daf55cc226b9baba4c5480f489427fdcefae72c2
mengjuhua <mengjuhua@risesoft.net>
2024-11-06 14:37:58 +0800

    refactor：添加防抖函数

--- vue/y9vue-kernel-standard/src/utils/index.ts
+++ vue/y9vue-kernel-standard/src/utils/index.ts
@@ -50,6 +50,52 @@ export const randomString = (e) => {
 
 /**
  * 防抖函数
+ * 多次连续点击时，只执行第一次的点击
+ * 使用示例 __debounce(fun,wait)  fun:事件处理函数， wait:延迟时间
+ * @returns {String}
+ * @author Yehaifeng
+ */
+export const __debounce = (fun: Function, wait: Number): Function => {
+    let timer,
+        count = 0; //维护全局纯净，借助闭包来实现
+    return function () {
+        if (!count) {
+            count++;
+            fun();
+        } else {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(function () {
+                //timer有值为真，这个事件已经触发了一次，重新开始计数
+                count = 0;
+            }, wait);
+        }
+    };
+};
+
+/**
+ * 防抖函数
+ * 多次连续点击时，只执行最后一次的点击
+ * 使用示例 debounce__(fun,wait)  fun:事件处理函数， wait:延迟时间
+ * @returns {String}
+ * @author Yehaifeng
+ */
+export const debounce__ = (fun: Function, wait: Number): Function => {
+    var timer; //维护全局纯净，借助闭包来实现
+    return function () {
+        if (timer) {
+            //timer有值为真，这个事件已经触发了一次，重新开始计数
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function () {
+            fun();
+        }, wait);
+    };
+};
+
+/**
+ * 防抖函数
  * 使用示例 debounce(fun,wait)  fun:事件处理函数， wait:延迟时间
  * @returns {String}
  * @author Yehaifeng
@@ -66,22 +112,6 @@ export const debounce = (fun: Function, wait: number): Function => {
         }, wait);
     };
 };
-
-/**
- * 生成guid函数
- * 使用示例 uuid("xxxx-yyyy-xx-yy")
- * @params guidFormat 传入guid字符串的输出模版 传入 xx-yy 输出 e0-6k
- * @returns {String}
- * @author Yehaifeng
- */
-export function uuid(guidFormat = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx') {
-    let guid = guidFormat.replace(/[xy]/g, function (c) {
-        var r = (Math.random() * 16) | 0,
-            v = c == 'x' ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-    });
-    return guid;
-}
 
 /**字体像素设置函数 */
 export function getConcreteSize(fontSize: string, actualValue: number) {
