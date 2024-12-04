53888770b9dbac33244014cdd492ed5c10a41590
shidaobang <shidaobang@risesoft.net>
2024-12-04 15:41:05 +0800

    feat: 记录失败的登录日志
    (cherry picked from commit 27d83dd6a5bf00c573f8aeb713252e50a6ca3f61)

--- y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-server-jpa/src/main/java/org/apereo/cas/authentication/RiseAuthenticationHandler.java
+++ y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-server-jpa/src/main/java/org/apereo/cas/authentication/RiseAuthenticationHandler.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 import javax.security.auth.login.AccountNotFoundException;
 import javax.security.auth.login.FailedLoginException;
-
-import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.authentication.credential.RememberMeUsernamePasswordCredential;
--- y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-server-jpa/src/main/java/y9/Y9Config.java
+++ y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-server-jpa/src/main/java/y9/Y9Config.java
@@ -2,7 +2,8 @@ package y9;
 
 import java.util.List;
 
-import jakarta.servlet.DispatcherType;
+import javax.servlet.DispatcherType;
+import javax.sql.DataSource;
 
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
 import org.apereo.cas.authentication.RiseAuthenticationHandler;
@@ -14,12 +15,13 @@ import org.apereo.cas.authentication.principal.RisePersonDirectoryPrincipalResol
 import org.apereo.cas.jpa.JpaPersistenceProviderConfigurer;
 import org.apereo.cas.services.JpaRegisteredServiceEntity;
 import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.services.Y9KeyValue;
 import org.apereo.cas.services.Y9LoginUser;
 import org.apereo.cas.services.Y9User;
 import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.spring.beans.BeanCondition;
 import org.apereo.cas.util.spring.beans.BeanSupplier;
-import org.apereo.cas.web.CasWebSecurityConfigurer;
+import org.apereo.cas.web.ProtocolEndpointWebSecurityConfigurer;
 import org.apereo.cas.web.flow.actions.RiseCredentialNonInteractiveCredentialsAction;
 import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
@@ -32,13 +34,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.Ordered;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.integration.transaction.PseudoTransactionManager;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionOperations;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.webflow.execution.Action;
@@ -49,7 +53,6 @@ import y9.service.Y9KeyValueService;
 import y9.service.Y9LoginUserService;
 import y9.service.Y9UserService;
 import y9.service.impl.Y9JpaKeyValueServiceImpl;
-import y9.service.impl.Y9RedisKeyValueServiceImpl;
 import y9.util.Y9Context;
 
 @Lazy(false)
@@ -57,22 +60,35 @@ import y9.util.Y9Context;
 public class Y9Config {
 
     @Configuration
-    public static class Y9JpaConfig {
-        private static final BeanCondition CONDITION =
-            BeanCondition.on("cas.service-registry.jpa.enabled").isTrue().evenIfMissing();
+    public static class Y9AuthenticationConfiguration {
+        @Bean
+        public AuthenticationEventExecutionPlanConfigurer riseAuthenticationEventExecutionPlanConfigurer(
+            @Qualifier(ServicesManager.BEAN_NAME) final ServicesManager servicesManager,
+            PrincipalResolver risePersonDirectoryPrincipalResolver, Y9UserService y9UserService,
+            Y9LoginUserService y9LoginUserService) {
+            RiseAuthenticationHandler handler = new RiseAuthenticationHandler("y9AuthenticationHandler",
+                servicesManager, risePrincipalFactory(), 0, y9UserService, y9LoginUserService);
+            return plan -> plan.registerAuthenticationHandlerWithPrincipalResolver(handler,
+                risePersonDirectoryPrincipalResolver);
+        }
 
         @Bean
-        @RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
-        public JpaPersistenceProviderConfigurer
-            jpaServicePersistenceProviderConfigurer(final ConfigurableApplicationContext applicationContext) {
-            return BeanSupplier.of(JpaPersistenceProviderConfigurer.class)
-                .when(CONDITION.given(applicationContext.getEnvironment())).supply(() -> context -> {
-                    String s1 = JpaRegisteredServiceEntity.class.getName();
-                    String s2 = Y9LoginUser.class.getName();
-                    String s3 = Y9User.class.getName();
-                    val entities = CollectionUtils.wrapList(s1, s2, s3);
-                    context.getIncludeEntityClasses().addAll(entities);
-                }).otherwiseProxy().get();
+        public Action riseCredentialNonInteractiveCredentialsAction(
+            @Qualifier("adaptiveAuthenticationPolicy") final AdaptiveAuthenticationPolicy adaptiveAuthenticationPolicy,
+            @Qualifier("serviceTicketRequestWebflowEventResolver") final CasWebflowEventResolver serviceTicketRequestWebflowEventResolver,
+            @Qualifier("initialAuthenticationAttemptWebflowEventResolver") final CasDelegatingWebflowEventResolver initialAuthenticationAttemptWebflowEventResolver) {
+            return new RiseCredentialNonInteractiveCredentialsAction(initialAuthenticationAttemptWebflowEventResolver,
+                serviceTicketRequestWebflowEventResolver, adaptiveAuthenticationPolicy);
+        }
+
+        @Bean
+        public PrincipalResolver risePersonDirectoryPrincipalResolver() {
+            return new RisePersonDirectoryPrincipalResolver();
+        }
+
+        @Bean
+        public PrincipalFactory risePrincipalFactory() {
+            return new DefaultPrincipalFactory();
         }
     }
 
@@ -98,8 +114,8 @@ public class Y9Config {
         }
 
         @Bean
-        public CasWebSecurityConfigurer<Void> y9ResourceConfigurer() {
-            return new CasWebSecurityConfigurer<>() {
+        public ProtocolEndpointWebSecurityConfigurer<Void> y9ResourceConfigurer() {
+            return new ProtocolEndpointWebSecurityConfigurer<>() {
                 @Override
                 public List<String> getIgnoredEndpoints() {
                     return List.of("/y9static/**", "/api/**");
@@ -109,62 +125,45 @@ public class Y9Config {
     }
 
     @Configuration
-    public static class Y9AuthenticationConfiguration {
-        @Bean
-        public AuthenticationEventExecutionPlanConfigurer riseAuthenticationEventExecutionPlanConfigurer(
-            @Qualifier(ServicesManager.BEAN_NAME) final ServicesManager servicesManager, Y9UserService y9UserService,
-            Y9LoginUserService y9LoginUserService) {
-            RiseAuthenticationHandler handler =
-                new RiseAuthenticationHandler("y9AuthenticationHandler", servicesManager, risePrincipalFactory(), 0);
-            return plan -> plan.registerAuthenticationHandlerWithPrincipalResolver(handler,
-                risePersonDirectoryPrincipalResolver());
-        }
+    public static class Y9JpaConfig {
+        private static final BeanCondition CONDITION =
+            BeanCondition.on("cas.service-registry.jpa.enabled").isTrue().evenIfMissing();
+        private static final BeanCondition CONDITION2 =
+            BeanCondition.on("cas.ticket.registry.jpa.enabled").isTrue().evenIfMissing();
 
         @Bean
-        public Action riseCredentialNonInteractiveCredentialsAction(
-            @Qualifier("adaptiveAuthenticationPolicy") final AdaptiveAuthenticationPolicy adaptiveAuthenticationPolicy,
-            @Qualifier("serviceTicketRequestWebflowEventResolver") final CasWebflowEventResolver serviceTicketRequestWebflowEventResolver,
-            @Qualifier("initialAuthenticationAttemptWebflowEventResolver") final CasDelegatingWebflowEventResolver initialAuthenticationAttemptWebflowEventResolver) {
-            return new RiseCredentialNonInteractiveCredentialsAction(initialAuthenticationAttemptWebflowEventResolver,
-                serviceTicketRequestWebflowEventResolver, adaptiveAuthenticationPolicy);
+        @RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
+        public JpaPersistenceProviderConfigurer
+            jpaServicePersistenceProviderConfigurer(final ConfigurableApplicationContext applicationContext) {
+            return BeanSupplier.of(JpaPersistenceProviderConfigurer.class)
+                .when(CONDITION.given(applicationContext.getEnvironment())).supply(() -> context -> {
+                    String s1 = JpaRegisteredServiceEntity.class.getName();
+                    String s2 = Y9LoginUser.class.getName();
+                    String s3 = Y9User.class.getName();
+                    String s4 = Y9KeyValue.class.getName();
+                    val entities = CollectionUtils.wrapList(s1, s2, s3, s4);
+                    context.getIncludeEntityClasses().addAll(entities);
+                }).otherwiseProxy().get();
         }
 
+        @Primary
         @Bean
-        public PrincipalResolver risePersonDirectoryPrincipalResolver() {
-            return new RisePersonDirectoryPrincipalResolver();
-        }
-
-        @Bean
-        public PrincipalFactory risePrincipalFactory() {
-            return new DefaultPrincipalFactory();
+        @RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
+        public PlatformTransactionManager jdbcLockTransactionManager(
+            final ConfigurableApplicationContext applicationContext,
+            @Qualifier("dataSourceTicket") DataSource dataSourceTicket) {
+            return BeanSupplier.of(PlatformTransactionManager.class)
+                .when(CONDITION2.given(applicationContext.getEnvironment()))
+                .supply(() -> new DataSourceTransactionManager(dataSourceTicket))
+                .otherwise(PseudoTransactionManager::new).get();
         }
     }
 
     @Configuration(proxyBeanMethods = false)
     public static class Y9KeyValueConfiguration {
-
         @Bean
         public Y9Context y9Context() {
             return new Y9Context();
-        }
-
-        @Configuration(proxyBeanMethods = false)
-        @ConditionalOnProperty(name = "cas.ticket.registry.redis.enabled", havingValue = "true", matchIfMissing = false)
-        public static class Y9RedisKeyValueConfiguration {
-
-            @Bean
-            public RedisTemplate<Object, Object> y9RedisTemplate(
-                @Qualifier("redisTicketConnectionFactory") final RedisConnectionFactory redisConnectionFactory) {
-                RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-                redisTemplate.setConnectionFactory(redisConnectionFactory);
-                return redisTemplate;
-            }
-
-            @Bean
-            public Y9KeyValueService
-                y9RedisKeyValueService(@Qualifier("y9RedisTemplate") RedisTemplate<Object, Object> y9RedisTemplate) {
-                return new Y9RedisKeyValueServiceImpl(y9RedisTemplate);
-            }
         }
 
         @Configuration(proxyBeanMethods = false)
