baa250931e4601ff05f26a33983bf282531a4cca
mengjuhua <mengjuhua@risesoft.net>
2025-01-07 10:03:40 +0800

    refactor：移除版本设置

--- risenet-gradle-build-logic/src/main/kotlin/net.risesoft.y9.javaPlatform-publish.gradle.kts
+++ risenet-gradle-build-logic/src/main/kotlin/net.risesoft.y9.javaPlatform-publish.gradle.kts
@@ -5,14 +5,6 @@ plugins {
     id("net.risesoft.y9.repository")
 }
 
-val versionCatalog = versionCatalogs.named("libs")
-val y9version = versionCatalog.findVersion("y9-version")
-if (y9version.isPresent) {
-    version = y9version.get().displayName
-} else {
-    version = "9.7.0-SNAPSHOT"
-}
-
 publishing {
     publications {
         repositories {
