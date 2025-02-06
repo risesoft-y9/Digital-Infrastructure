5d0696deae86303f237e0b8818e1176daa262fd4
shidaobang <shidaobang@risesoft.net>
2025-02-06 10:50:32 +0800

    refactor: 移除无用的配置

--- y9-digitalbase-module/y9-module-log/risenet-y9boot-webapp-log/src/main/java/net/risesoft/config/WebMvcConfig.java
+++ y9-digitalbase-module/y9-module-log/risenet-y9boot-webapp-log/src/main/java/net/risesoft/config/WebMvcConfig.java
@@ -1,30 +1,13 @@
 package net.risesoft.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import net.risesoft.util.EsIndexDate;
 import net.risesoft.y9.Y9Context;
 
 @Configuration
 public class WebMvcConfig {
-
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        List<MediaType> supportedMediaTypes = new ArrayList<>();
-        supportedMediaTypes.add(MediaType.parseMediaType("text/html;charset=UTF-8"));
-        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
-        supportedMediaTypes.add(new MediaType("application", "*+json"));
-        converter.setSupportedMediaTypes(supportedMediaTypes);
-        return converter;
-    }
 
     @Bean
     public Y9Context y9Context() {
