1ad50b3d5b9844ebfd72b6dddcf7d81003dcd2f6
shidaobang <shidaobang@risesoft.net>
2025-03-20 13:59:59 +0800

    remove: 移除 6.6.9 版本的 sso-server

--- y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-server/src/main/java/y9/service/Y9LoginUserService.java
+++
@@ -1,9 +0,0 @@
-package y9.service;
-
-import org.apereo.cas.authentication.credential.RememberMeUsernamePasswordCredential;
-
-public interface Y9LoginUserService {
-
-    void save(final RememberMeUsernamePasswordCredential credential, String success, String logMessage);
-
-}
