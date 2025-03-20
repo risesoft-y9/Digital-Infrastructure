d10537edc3ab541d0bf204c71a2c9ec76fc4d6ac
qinman <qinman@risesoft.net>
2025-03-20 16:23:31 +0800

    add:新增组织接口

---
+++ y9-digitalbase-module/y9-module-platform/risenet-y9boot-api-interface-platform/src/main/java/net/risesoft/api/platform/org/dto/CreateOrganizationDTO.java
@@ -0,0 +1,41 @@
+package net.risesoft.api.platform.org.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 组织
+ *
+ * @author dingzhaojun
+ * @author qinman
+ * @author mengjuhua
+ * @author shidaobang
+ * @date 2022/4/21
+ */
+@Getter
+@Setter
+public class CreateOrganizationDTO extends CreateOrgUnitBaseDTO {
+
+    private static final long serialVersionUID = 7911871659478634602L;
+
+    /**
+     * 英文名称
+     */
+    private String enName;
+
+    /**
+     * 机构代码
+     */
+    private String organizationCode;
+
+    /**
+     * 组织机构类型
+     */
+    private String organizationType;
+
+    /**
+     * 机构类型,0=实体组织，1=虚拟组织
+     */
+    private Boolean virtual = false;
+
+}
