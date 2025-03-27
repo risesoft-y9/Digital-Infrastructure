a546fb8a3162a01e21e6d1c9d5914b2db5014fc3
mengjuhua <mengjuhua@risesoft.net>
2025-03-27 17:22:45 +0800

    fix: 处理合并后的问题

--- y9-digitalbase-module/y9-module-platform/risenet-y9boot-api-interface-platform/src/main/java/net/risesoft/api/platform/org/dto/CreateJobDTO.java
+++ y9-digitalbase-module/y9-module-platform/risenet-y9boot-api-interface-platform/src/main/java/net/risesoft/api/platform/org/dto/CreateJobDTO.java
@@ -1,6 +1,6 @@
 package net.risesoft.api.platform.org.dto;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 import lombok.Getter;
 import lombok.Setter;
