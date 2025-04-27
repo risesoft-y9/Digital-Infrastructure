9077bbd2cf9bd88c1c69747fc3ce24696e4b0c73
shidaobang <shidaobang@risesoft.net>
2025-04-27 11:16:00 +0800

    fix: 导入导出系统 JSON 增加系统下的角色
    (cherry picked from commit 811a2b2db3a19956701faf6faf77507e638402cb)

--- y9-digitalbase-module/y9-module-platform/risenet-y9boot-support-platform-service/src/main/java/net/risesoft/dataio/system/model/Y9RoleJsonModel.java
+++ y9-digitalbase-module/y9-module-platform/risenet-y9boot-support-platform-service/src/main/java/net/risesoft/dataio/system/model/Y9RoleJsonModel.java
@@ -20,12 +20,4 @@ public class Y9RoleJsonModel extends Y9Role {
     private static final long serialVersionUID = 5734021385079857067L;
 
     private List<Y9RoleJsonModel> subRoleList;
-
-    public List<Y9RoleJsonModel> getSubRoleList() {
-        return subRoleList;
-    }
-
-    public void setSubRoleList(List<Y9RoleJsonModel> subRoleList) {
-        this.subRoleList = subRoleList;
-    }
 }
