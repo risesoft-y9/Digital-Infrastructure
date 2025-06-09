409f1929da44155ee676002955b93d9b99761f33
shidaobang <shidaobang@risesoft.net>
2025-06-09 14:49:16 +0800

    revert: 恢复有使用的接口

--- y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-server/src/main/java/y9/controller/TenantController.java
+++ y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-server/src/main/java/y9/controller/TenantController.java
@@ -1,6 +1,7 @@
 package y9.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -20,7 +22,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import y9.controller.dto.Tenant;
+import y9.entity.Y9User;
 import y9.service.Y9UserService;
+import y9.util.MobileUtil;
+import y9.util.common.XSSCheckUtil;
 
 @Lazy(false)
 @Controller
@@ -39,6 +44,51 @@ public class TenantController {
 
             ObjectMapper mapper = new ObjectMapper();
             String jsonStr = mapper.writeValueAsString(tenants1);
+
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            return new ResponseEntity<>(jsonStr, headers, HttpStatus.OK);
+        } catch (final Throwable e) {
+            LOGGER.error(e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * 根据登录名，获取租户信息列表（目前手机端在用）
+     *
+     * @param loginName
+     * @return
+     */
+    @RequestMapping(value = "/tenants")
+    public final ResponseEntity<String> getTenants(@RequestParam String loginName) {
+        List<Map<String, Object>> list = new ArrayList<>();
+        try {
+            loginName = XSSCheckUtil.filter(loginName);
+            if (MobileUtil.isMobile(loginName)) {
+                List<Y9User> users = y9UserService.findByMobileAndOriginal(loginName, Boolean.TRUE);
+                if (users.size() > 0) {
+                    for (Y9User user : users) {
+                        Map<String, Object> mapTemp = new HashMap<>();
+                        mapTemp.put("tenantShortName", user.getTenantShortName());
+                        mapTemp.put("tenantName", user.getTenantName());
+                        list.add(mapTemp);
+                    }
+                }
+            } else {
+                List<Y9User> users = y9UserService.findByLoginNameAndOriginal(loginName, Boolean.TRUE);
+                if (!users.isEmpty()) {
+                    for (Y9User user : users) {
+                        Map<String, Object> mapTemp = new HashMap<>();
+                        mapTemp.put("tenantShortName", user.getTenantShortName());
+                        mapTemp.put("tenantName", user.getTenantName());
+                        list.add(mapTemp);
+                    }
+                }
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonStr = mapper.writeValueAsString(list);
 
             final HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
