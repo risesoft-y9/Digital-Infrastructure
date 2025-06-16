7c7b1cf3e5165a970c9386f6d7dca6565f9330f1
shidaobang <shidaobang@risesoft.net>
2025-06-16 11:27:30 +0800

    fix: 添加 UserApiClient

---
+++ y9-digitalbase-module/y9-module-platform/risenet-y9boot-api-feignclient-platform/src/main/java/y9/client/rest/platform/user/UserApiClient.java
@@ -0,0 +1,16 @@
+package y9.client.rest.platform.user;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+import net.risesoft.api.platform.user.UserApi;
+
+/**
+ * 用户 API
+ *
+ * @author shidaobang
+ * @date 2025/06/16
+ */
+@FeignClient(contextId = "UserApiClient", name = "${y9.service.org.name:platform}",
+    url = "${y9.service.org.directUrl:}", path = "/${y9.service.org.name:platform}/services/rest/v1/user",
+    primary = false)
+public interface UserApiClient extends UserApi {}
