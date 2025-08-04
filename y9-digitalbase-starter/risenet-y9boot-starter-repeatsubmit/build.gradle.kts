6225f4c43dd4c4f2510717ab3a6b579c9dd0be02
mengjuhua <mengjuhua@risesoft.net>
2025-08-04 11:19:39 +0800

    refactor: risenet-y9boot-starter-repeatsubmit

---
+++ y9-digitalbase-starter/risenet-y9boot-starter-repeatsubmit/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("net.risesoft.y9.conventions-java")
+    id("net.risesoft.y9.lombok")
+}
+
+dependencies {
+    api(project(":y9-digitalbase-common:risenet-y9boot-properties"))
+    api(project(":y9-digitalbase-common:risenet-y9boot-common-util"))
+    api(project(":y9-digitalbase-module:y9-module-platform:risenet-y9boot-api-feignclient-platform"))
+
+    api(platform(libs.spring.boot.bom))
+    api("org.springframework.boot:spring-boot-autoconfigure")
+    api("org.springframework.boot:spring-boot-configuration-processor")
+    api("org.springframework.boot:spring-boot-starter-data-redis")
+    api("com.github.ben-manes.caffeine:caffeine")
+    api(libs.checker.qual)
+    
+    compileOnly("jakarta.servlet:jakarta.servlet-api")
+}
+
+description = "risenet-y9boot-starter-repeatsubmit"
--- y9-digitalbase-starter/risenet-y9boot-starter-repeatsubmit/src/main/java/net/risesoft/permission/aop/RepeatSubmitAdvice.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-repeatsubmit/src/main/java/net/risesoft/permission/aop/RepeatSubmitAdvice.java
@@ -3,7 +3,7 @@ package net.risesoft.permission.aop;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.aop.MethodBeforeAdvice;
