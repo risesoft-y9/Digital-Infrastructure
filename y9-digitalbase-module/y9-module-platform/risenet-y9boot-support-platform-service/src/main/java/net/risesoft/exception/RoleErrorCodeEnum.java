f08913a0af9ae67056e86243f99118be50fc81cc
shidaobang <shidaobang@risesoft.net>
2025-08-25 14:51:38 +0800

    fix: 角色移动的父节点为应用时报错
    (cherry picked from commit 36e0588fec02e4bfacb3afe5a08006d09db0b881)

--- y9-digitalbase-module/y9-module-platform/risenet-y9boot-support-platform-service/src/main/java/net/risesoft/exception/RoleErrorCodeEnum.java
+++ y9-digitalbase-module/y9-module-platform/risenet-y9boot-support-platform-service/src/main/java/net/risesoft/exception/RoleErrorCodeEnum.java
@@ -13,12 +13,10 @@ import net.risesoft.consts.ErrorCodeConsts;
  */
 @RequiredArgsConstructor
 public enum RoleErrorCodeEnum implements ErrorCode {
-    /** role not found */
     ROLE_NOT_FOUND(0, "角色[{}]不存在"),
-    /** orgunit role not found */
     ORG_UNIT_ROLE_NOT_FOUND(1, "组织节点和角色的关联[{}]不存在"),
-    /** 当前角色的角色成员中已包含要添加的组织节点 */
-    ORG_UNIT_INCLUDED(2, "当前角色的角色成员中已包含要添加的组织节点[{}]");
+    ORG_UNIT_INCLUDED(2, "当前角色的角色成员中已包含要添加的组织节点[{}]"),
+    ROLE_PARENT_NOT_FOUND(3, "角色父节点[{}]不存在");
 
     private final int moduleErrorCode;
     private final String description;
