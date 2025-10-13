48c1bc326f55634f7111ec6404cbc69009f766f5
shidaobang <shidaobang@risesoft.net>
2025-10-13 09:49:38 +0800

    revert: 恢复 oauth2 退出

---
+++ y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso-jpa/src/main/java/org/apereo/cas/services/DefaultServicesManagerRegisteredServiceLocator.java
@@ -0,0 +1,52 @@
+package org.apereo.cas.services;
+
+import java.util.Collection;
+import java.util.function.BiPredicate;
+
+import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.support.oauth.services.OAuthRegisteredService;
+import org.springframework.core.Ordered;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.val;
+
+/**
+ * This is {@link DefaultServicesManagerRegisteredServiceLocator}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.3.0
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class DefaultServicesManagerRegisteredServiceLocator implements ServicesManagerRegisteredServiceLocator {
+    private int order = Ordered.LOWEST_PRECEDENCE;
+
+    private BiPredicate<RegisteredService, Service> registeredServiceFilter = (registeredService, service) -> {
+        val supportedType = supports(registeredService, service);
+        return supportedType && registeredService.matches(service.getId());
+    };
+
+    @Override
+    public RegisteredService locate(final Collection<RegisteredService> candidates, final Service service) {
+        return candidates.stream()
+            .filter(registeredService -> supports(registeredService, service))
+            .filter(registeredService -> registeredServiceFilter.test(registeredService, service))
+            .findFirst()
+            .orElse(null);
+    }
+
+    @Override
+    public boolean supports(final RegisteredService registeredService, final Service service) {
+        return (OAuthRegisteredService.class.isAssignableFrom(registeredService.getClass())
+            && registeredService.getFriendlyName().equalsIgnoreCase("OAuth2 Client"))
+            || (CasRegisteredService.class.isAssignableFrom(registeredService.getClass())
+                && registeredService.getFriendlyName().equalsIgnoreCase(CasRegisteredService.FRIENDLY_NAME))
+            || (RegexRegisteredService.class.isAssignableFrom(registeredService.getClass())
+                && registeredService.getFriendlyName().equalsIgnoreCase(CasRegisteredService.FRIENDLY_NAME));
+    }
+}
---
+++ y9-digitalbase-module/y9-module-sso/risenet-y9boot-webapp-sso/src/main/java/org/apereo/cas/logout/slo/DefaultSingleLogoutServiceLogoutUrlBuilder.java
@@ -0,0 +1,35 @@
+package org.apereo.cas.logout.slo;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apereo.cas.authentication.principal.WebApplicationService;
+import org.apereo.cas.services.CasRegisteredService;
+import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.web.UrlValidator;
+
+/**
+ * This is {@link DefaultSingleLogoutServiceLogoutUrlBuilder} which acts on a registered service to determine how the
+ * logout url endpoint should be decided.
+ *
+ * @author Misagh Moayyed
+ * @since 5.0.0
+ */
+public class DefaultSingleLogoutServiceLogoutUrlBuilder extends BaseSingleLogoutServiceLogoutUrlBuilder {
+    public DefaultSingleLogoutServiceLogoutUrlBuilder(
+        final ServicesManager servicesManager,
+        final UrlValidator urlValidator) {
+        super(servicesManager, urlValidator);
+    }
+
+    @Override
+    public boolean supports(final RegisteredService registeredService, final WebApplicationService singleLogoutService,
+        final Optional<HttpServletRequest> httpRequest) {
+        return super.supports(registeredService, singleLogoutService, httpRequest)
+            && (registeredService.getFriendlyName().equalsIgnoreCase(CasRegisteredService.FRIENDLY_NAME)
+                || registeredService.getFriendlyName().equalsIgnoreCase("OAuth2 Client"));
+    }
+
+}
