58c5fe185f2c6cb646713e04962a72b18f2cf0bd
shidaobang <shidaobang@risesoft.net>
2025-11-10 16:23:29 +0800

    fix: 前台提交的时间参数不能正确转换成 Date 的问题
    (cherry picked from commit 28be952d820d98a06ecad42234c5b1e413668503)

--- y9-digitalbase-starter/risenet-y9boot-starter-web/src/main/java/y9/autoconfiguration/web/Y9WebConfiguration.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-web/src/main/java/y9/autoconfiguration/web/Y9WebConfiguration.java
@@ -1,14 +1,19 @@
 package y9.autoconfiguration.web;
 
+import java.util.Date;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.format.datetime.DateFormatter;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import net.risesoft.web.Y9LaxStringToEnumConverterFactory;
 import net.risesoft.web.handler.Y9ControllerAdvice;
+
+import cn.hutool.core.date.DateUtil;
 
 /**
  * web 配置
@@ -27,7 +32,20 @@ public class Y9WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new MultiFormatDateConverter());
         registry.addConverterFactory(new Y9LaxStringToEnumConverterFactory());
-        registry.addFormatter(new DateFormatter("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    /**
+     * 支持多种格式的日期转换器
+     */
+    static class MultiFormatDateConverter implements Converter<String, Date> {
+        @Override
+        public Date convert(String source) {
+            if (StringUtils.hasText(source)) {
+                return DateUtil.parse(source.trim());
+            }
+            return null;
+        }
     }
 }
