eab75120ba75fb582bdc40f743f343f1a73504c5
mengjuhua <mengjuhua@risesoft.net>
2025-11-12 15:05:27 +0800

    fix：处理CustomGroupMemberQuery传参错误的问题
    (cherry picked from commit bf71f3eb26c3e909dc3f3a7350ddaeca993321ad)

--- y9-digitalbase-module/y9-module-platform/risenet-y9boot-common-platform/src/main/java/net/risesoft/query/platform/CustomGroupMemberQuery.java
+++ y9-digitalbase-module/y9-module-platform/risenet-y9boot-common-platform/src/main/java/net/risesoft/query/platform/CustomGroupMemberQuery.java
@@ -2,10 +2,11 @@ package net.risesoft.query.platform;
 
 import jakarta.validation.constraints.NotBlank;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import net.risesoft.enums.platform.org.OrgTypeEnum;
 
@@ -17,15 +18,15 @@ import net.risesoft.enums.platform.org.OrgTypeEnum;
  */
 @Data
 @Builder
-@RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CustomGroupMemberQuery {
 
     /**
      * 用户组 id
      */
     @NotBlank
-    private final String groupId;
+    private String groupId;
 
     /**
      * 成员类型
