c6c58d577475b7810c29dbb1dd0531bdf9ff79c6
shidaobang <shidaobang@risesoft.net>
2025-12-05 17:15:36 +0800

    refactor: risenet-y9boot-starter-liquibase 中关于多租户的逻辑移动到 risenet-y9boot-starter-multi-tenant
    (cherry picked from commit 02a948124e98c150c0ff7ce1aba2e8031babfd50)

--- y9-digitalbase-starter/risenet-y9boot-starter-liquibase/src/main/java/net/risesoft/liquibase/Y9MultiTenantSpringLiquibase.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-liquibase/src/main/java/net/risesoft/liquibase/Y9MultiTenantSpringLiquibase.java
@@ -1,17 +1,14 @@
 package net.risesoft.liquibase;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.core.io.ResourceLoader;
+import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.core.io.ResourceLoader;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import net.risesoft.y9.configuration.feature.liquibase.Y9LiquibaseProperties;
-import net.risesoft.y9.tenant.datasource.Y9TenantDataSourceLookup;
 
 import liquibase.exception.LiquibaseException;
 import liquibase.integration.spring.SpringLiquibase;
@@ -23,30 +20,12 @@ import liquibase.integration.spring.SpringLiquibase;
  */
 @RequiredArgsConstructor
 @Slf4j
-public class Y9MultiTenantSpringLiquibase implements InitializingBean {
+public class Y9MultiTenantSpringLiquibase {
 
-    private final Y9TenantDataSourceLookup y9TenantDataSourceLookup;
     private final Y9LiquibaseProperties properties;
     private final ResourceLoader resourceLoader;
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        // updateAll();
-    }
-
-    /**
-     * 更新所有租户数据源的表结构
-     *
-     */
-    public void updateAll() {
-        Map<String, HikariDataSource> dataSources = y9TenantDataSourceLookup.getDataSources();
-        for (Map.Entry<String, HikariDataSource> stringDruidDataSourceEntry : dataSources.entrySet()) {
-            HikariDataSource dataSource = stringDruidDataSourceEntry.getValue();
-            update(dataSource);
-        }
-    }
-
-    private void update(HikariDataSource dataSource) {
+    public void update(HikariDataSource dataSource) {
         try {
             SpringLiquibase liquibase =
                 LiquibaseUtil.getSpringLiquibase(dataSource, this.properties, this.resourceLoader, true);
@@ -54,17 +33,6 @@ public class Y9MultiTenantSpringLiquibase implements InitializingBean {
         } catch (LiquibaseException e) {
             LOGGER.warn("更新表结构异常", e);
         }
-    }
-
-    /**
-     * 更新单个租户数据源的表结构
-     *
-     * @param tenantId 租户id
-     */
-    public void update(String tenantId) {
-        // 方法暴露出去 工程中可调用执行
-        HikariDataSource dataSource = (HikariDataSource)y9TenantDataSourceLookup.getDataSource(tenantId);
-        update(dataSource);
     }
 
 }
--- y9-digitalbase-starter/risenet-y9boot-starter-liquibase/src/main/java/y9/autoconfiguration/liquibase/Y9LiquibaseAutoConfiguration.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-liquibase/src/main/java/y9/autoconfiguration/liquibase/Y9LiquibaseAutoConfiguration.java
@@ -1,5 +1,7 @@
 package y9.autoconfiguration.liquibase;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -15,7 +17,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import net.risesoft.liquibase.LiquibaseUtil;
 import net.risesoft.liquibase.Y9MultiTenantSpringLiquibase;
 import net.risesoft.y9.configuration.feature.liquibase.Y9LiquibaseProperties;
-import net.risesoft.y9.tenant.datasource.Y9TenantDataSourceLookup;
 
 import liquibase.integration.spring.SpringLiquibase;
 
@@ -30,17 +31,16 @@ import liquibase.integration.spring.SpringLiquibase;
 public class Y9LiquibaseAutoConfiguration {
 
     @Bean
-    @ConditionalOnBean(name = "y9TenantDataSourceLookup")
     @ConditionalOnProperty(name = "y9.feature.liquibase.tenant-enabled", havingValue = "true")
-    public Y9MultiTenantSpringLiquibase y9MultiTenantSpringLiquibase(Y9TenantDataSourceLookup y9TenantDataSourceLookup,
-        Y9LiquibaseProperties properties, ResourceLoader resourceLoader) {
-        return new Y9MultiTenantSpringLiquibase(y9TenantDataSourceLookup, properties, resourceLoader);
+    public Y9MultiTenantSpringLiquibase y9MultiTenantSpringLiquibase(Y9LiquibaseProperties properties,
+        ResourceLoader resourceLoader) {
+        return new Y9MultiTenantSpringLiquibase(properties, resourceLoader);
     }
 
     @Bean
     @ConditionalOnBean(name = "y9PublicDS")
-    public SpringLiquibase liquibase(Y9LiquibaseProperties properties,
-        @Qualifier("y9PublicDS") HikariDataSource dataSource, ResourceLoader resourceLoader) {
+    public SpringLiquibase liquibase(Y9LiquibaseProperties properties, @Qualifier("y9PublicDS") HikariDataSource dataSource,
+        ResourceLoader resourceLoader) {
         return LiquibaseUtil.getSpringLiquibase(dataSource, properties, resourceLoader, false);
     }
 
