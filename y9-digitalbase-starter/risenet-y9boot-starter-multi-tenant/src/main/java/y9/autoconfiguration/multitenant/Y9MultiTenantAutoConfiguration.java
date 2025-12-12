2301e16c2f42079a93eaad36a6e054709d2a24f8
shidaobang <shidaobang@risesoft.net>
2025-12-12 16:23:25 +0800

    fix: 添加 Y9Properties bean 的注册

--- y9-digitalbase-starter/risenet-y9boot-starter-multi-tenant/src/main/java/y9/autoconfiguration/multitenant/Y9MultiTenantAutoConfiguration.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-multi-tenant/src/main/java/y9/autoconfiguration/multitenant/Y9MultiTenantAutoConfiguration.java
@@ -48,7 +48,7 @@ import net.risesoft.y9.tenant.datasource.Y9TenantDataSourceLookup;
  * @date 2023/12/05
  * @since 9.6.3
  */
-@EnableConfigurationProperties(Y9MultiTenantProperties.class)
+@EnableConfigurationProperties({Y9MultiTenantProperties.class, Y9Properties.class})
 @RequiredArgsConstructor
 @Slf4j
 @AutoConfiguration
