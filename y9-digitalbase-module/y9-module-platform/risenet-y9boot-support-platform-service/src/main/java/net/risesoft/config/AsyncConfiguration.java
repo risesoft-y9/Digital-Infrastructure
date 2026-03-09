f0f43d2ab380a3177cf1a7581bc9d0bae1d979c1
shidaobang <shidaobang@risesoft.net>
2026-03-09 16:41:26 +0800

    fix: (platform) 异步任务在使用线程池复用线程的执行组件情况下没有传递 ThreadLocal 值的问题

---
+++ y9-digitalbase-module/y9-module-platform/risenet-y9boot-support-platform-service/src/main/java/net/risesoft/config/AsyncConfiguration.java
@@ -0,0 +1,35 @@
+package net.risesoft.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.alibaba.ttl.threadpool.TtlExecutors;
+
+/**
+ * 异步任务配置
+ *
+ * @author shidaobang
+ * @date 2026/03/09
+ */
+@Configuration
+@EnableAsync(proxyTargetClass = true)
+public class AsyncConfiguration implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(16);
+        executor.setQueueCapacity(1000);
+        executor.setThreadNamePrefix("platform-task-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return TtlExecutors.getTtlExecutor(executor);
+    }
+
+}
