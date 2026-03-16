772afa16bdc91c3ff8730d3763bb885a582472be
shidaobang <shidaobang@risesoft.net>
2026-03-16 17:06:03 +0800

    fix: 系统部署多个实例情况下，系统被租用时只有其中一个实例的数据源刷新的问题
    (cherry picked from commit 6934b9e44cf570614469b525661685e81ba7d8f4)

--- y9-digitalbase-starter/risenet-y9boot-starter-multi-tenant/src/main/java/net/risesoft/eventsource/KafkaMessageCommon.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-multi-tenant/src/main/java/net/risesoft/eventsource/KafkaMessageCommon.java
@@ -31,19 +31,47 @@ public class KafkaMessageCommon {
             String eventType = msg.getEventType();
 
             if ((Y9CommonEventConst.TENANT_SYSTEM_REGISTERED.equals(eventType)
-                || Y9CommonEventConst.TENANT_APP_REGISTERED.equals(eventType)
-                || Y9CommonEventConst.TENANT_DATASOURCE_SYNC.equals(eventType))
-                && !Objects.equals(Y9Context.getSystemName(), msg.getEventTarget())) {
+                || Y9CommonEventConst.TENANT_APP_REGISTERED.equals(eventType))
+                && Objects.equals(Y9Context.getSystemName(), msg.getEventTarget())) {
                 // 对于非当前引入的系统的消息不处理
-                return;
+                
+                Y9EventCommon event = new Y9EventCommon();
+                event.setEventType(msg.getEventType());
+                event.setEventObject(msg.getEventObject());
+                event.setTarget(msg.getEventTarget());
+                Y9Context.publishEvent(event);
+                LOGGER.info("[common]将消息中间件发过来的消息转换成spring的事件后发送：{}", event);
             }
 
-            Y9EventCommon event = new Y9EventCommon();
-            event.setEventType(msg.getEventType());
-            event.setEventObject(msg.getEventObject());
-            event.setTarget(msg.getEventTarget());
-            Y9Context.publishEvent(event);
-            LOGGER.info("[common]将消息中间件发过来的消息转换成spring的事件后发送：{}", event);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+
+    /**
+     * 监听租户数据源同步事件，这里的 groupId 使用随机生成保证引入服务在多实例时都能监听到 <br>
+     * 可根据实际情况调整 kafka 的 offsets.retention.minutes （无活跃成员的消费者组的保留时长）配置项清理无用的消费者组
+     */
+    @KafkaListener(topics = {"y9_common_event"},
+        groupId = "${spring.kafka.consumer.group-id}-${random.uuid}")
+    public void dataSourceSyncListener4kafka(ConsumerRecord<String, String> data) {
+        String value = data.value();
+        try {
+            Y9MessageCommon msg = Y9JsonUtil.readValue(value, Y9MessageCommon.class);
+            String eventType = msg.getEventType();
+
+            if (Y9CommonEventConst.TENANT_DATASOURCE_SYNC.equals(eventType)
+                && Objects.equals(Y9Context.getSystemName(), msg.getEventTarget())) {
+                // 对于非当前引入的系统的消息不处理
+
+                Y9EventCommon event = new Y9EventCommon();
+                event.setEventType(msg.getEventType());
+                event.setEventObject(msg.getEventObject());
+                event.setTarget(msg.getEventTarget());
+                Y9Context.publishEvent(event);
+                LOGGER.info("[common]将消息中间件发过来的消息转换成spring的事件后发送：{}", event);
+            }
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
