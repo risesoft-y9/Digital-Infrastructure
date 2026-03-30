1306d88e418ae1320e03b5ac89def87a8878cf88
shidaobang <shidaobang@risesoft.net>
2026-03-30 15:55:18 +0800

    refactor: redis 的值使用 Jackson 代替 Jdk 的序列化、反序列化

--- y9-digitalbase-starter/risenet-y9boot-starter-cache-redis/src/main/java/y9/autoconfiguration/cache/redis/Y9CacheRedisConfiguration.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-cache-redis/src/main/java/y9/autoconfiguration/cache/redis/Y9CacheRedisConfiguration.java
@@ -1,6 +1,5 @@
 package y9.autoconfiguration.cache.redis;
 
-import java.lang.reflect.Method;
 import java.util.LinkedHashSet;
 import java.util.List;
 
@@ -17,13 +16,12 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
 @Configuration
@@ -38,14 +36,11 @@ public class Y9CacheRedisConfiguration extends CachingConfigurerSupport {
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
 
-    @Autowired
-    private ResourceLoader resourceLoader;
-
     @Override
     @Bean
     public CacheManager cacheManager() {
         RedisCacheManagerBuilder builder =
-            RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(determineConfiguration(resourceLoader));
+            RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(determineConfiguration());
         List<String> cacheNames = this.cacheProperties.getCacheNames();
         if (!cacheNames.isEmpty()) {
             builder.initialCacheNames(new LinkedHashSet<>(cacheNames));
@@ -53,11 +48,10 @@ public class Y9CacheRedisConfiguration extends CachingConfigurerSupport {
         return builder.build();
     }
 
-    private RedisCacheConfiguration determineConfiguration(ResourceLoader resourceLoader) {
+    private RedisCacheConfiguration determineConfiguration() {
         Redis redisProperties = this.cacheProperties.getRedis();
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
-        config = config.serializeValuesWith(
-            SerializationPair.fromSerializer(new JdkSerializationRedisSerializer(resourceLoader.getClassLoader())));
+        config = config.serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
         if (redisProperties.getTimeToLive() != null) {
             config = config.entryTtl(redisProperties.getTimeToLive());
         }
@@ -78,19 +72,16 @@ public class Y9CacheRedisConfiguration extends CachingConfigurerSupport {
     public KeyGenerator keyGenerator() {
         // return new SimpleKeyGenerator();
 
-        return new KeyGenerator() {
-            @Override
-            public Object generate(Object target, Method method, Object... params) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(target.getClass().getName() + ":");
-                sb.append(method.getName() + ":");
-                for (Object param : params) {
-                    if (param != null) {
-                        sb.append(param.toString() + ":");
-                    }
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName() + ":");
+            sb.append(method.getName() + ":");
+            for (Object param : params) {
+                if (param != null) {
+                    sb.append(param.toString() + ":");
                 }
-                return sb.toString().substring(0, sb.length() - 1);
             }
+            return sb.toString().substring(0, sb.length() - 1);
         };
     }
 
