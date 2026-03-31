67336e3d2f90dfb6f1f0013e5f49553142e19de2
shidaobang <shidaobang@risesoft.net>
2026-03-31 17:46:02 +0800

    fix: Jackson 反序列化忽略类中不存在的字段

--- y9-digitalbase-starter/risenet-y9boot-starter-cache-redis/src/main/java/y9/autoconfiguration/cache/redis/Y9CacheRedisConfiguration.java
+++ y9-digitalbase-starter/risenet-y9boot-starter-cache-redis/src/main/java/y9/autoconfiguration/cache/redis/Y9CacheRedisConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.KotlinDetector;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
@@ -23,6 +24,15 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.util.ClassUtils;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.impl.StdTypeResolverBuilder;
 
 @Configuration
 @EnableConfigurationProperties(CacheProperties.class)
@@ -51,7 +61,18 @@ public class Y9CacheRedisConfiguration extends CachingConfigurerSupport {
     private RedisCacheConfiguration determineConfiguration() {
         Redis redisProperties = this.cacheProperties.getRedis();
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
-        config = config.serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        StdTypeResolverBuilder typer = new TypeResolverBuilder(ObjectMapper.DefaultTyping.EVERYTHING,
+                objectMapper.getPolymorphicTypeValidator());
+        typer = typer.init(JsonTypeInfo.Id.CLASS, null);
+        typer = typer.inclusion(JsonTypeInfo.As.PROPERTY);
+        objectMapper.setDefaultTyping(typer);
+
+        config = config.serializeValuesWith(
+            SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
+
         if (redisProperties.getTimeToLive() != null) {
             config = config.entryTtl(redisProperties.getTimeToLive());
         }
@@ -93,4 +114,60 @@ public class Y9CacheRedisConfiguration extends CachingConfigurerSupport {
         return redisTemplate;
     }
 
+    private static class TypeResolverBuilder extends ObjectMapper.DefaultTypeResolverBuilder {
+
+        public TypeResolverBuilder(ObjectMapper.DefaultTyping t, PolymorphicTypeValidator ptv) {
+            super(t, ptv);
+        }
+
+        @Override
+        public ObjectMapper.DefaultTypeResolverBuilder withDefaultImpl(Class<?> defaultImpl) {
+            return this;
+        }
+
+        /**
+         * Method called to check if the default type handler should be used for given type. Note: "natural types" (String,
+         * Boolean, Integer, Double) will never use typing; that is both due to them being concrete and final, and since
+         * actual serializers and deserializers will also ignore any attempts to enforce typing.
+         */
+        public boolean useForType(JavaType t) {
+
+            if (t.isJavaLangObject()) {
+                return true;
+            }
+
+            t = resolveArrayOrWrapper(t);
+
+            if (t.isEnumType() || ClassUtils.isPrimitiveOrWrapper(t.getRawClass())) {
+                return false;
+            }
+
+            if (t.isFinal() && !KotlinDetector.isKotlinType(t.getRawClass())
+                    && t.getRawClass().getPackage().getName().startsWith("java")) {
+                return false;
+            }
+
+            // [databind#88] Should not apply to JSON tree models:
+            return !TreeNode.class.isAssignableFrom(t.getRawClass());
+        }
+
+        private JavaType resolveArrayOrWrapper(JavaType type) {
+
+            while (type.isArrayType()) {
+                type = type.getContentType();
+                if (type.isReferenceType()) {
+                    type = resolveArrayOrWrapper(type);
+                }
+            }
+
+            while (type.isReferenceType()) {
+                type = type.getReferencedType();
+                if (type.isArrayType()) {
+                    type = resolveArrayOrWrapper(type);
+                }
+            }
+
+            return type;
+        }
+    }
 }
